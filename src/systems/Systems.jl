f9a8f72da9dae25cd972eba3d5f89e648ef0fc8d
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-08-23 23:24:26 +0200

    Implemented one of the Anischenki maps

--- src/systems/Systems.jl
+++ src/systems/Systems.jl
@@ -10,11 +10,13 @@ import DynamicalSystemsBase:
 
 import DifferentialEquations: @ode_def
 
+include("discretemaps/anishchenko1.jl")
 include("discretemaps/henon2.jl")
 include("discretemaps/logistic2.jl")
 include("discretemaps/logistic3.jl")
 include("discretemaps/var1.jl")
 
+include("continuous_systems/rosslerrossler.jl")
 
 # We export both the equations of motion and function to generate
 # the systems for every example. The equations of motion functions
@@ -23,24 +25,27 @@ export
 ###############
 # Discrete maps
 ###############
-eom_henon2, henon2
-eom_logistic2, logistic2
-eom_logistic3, logistic3
+eom_anishchenko1, anishchenko1,
+eom_henon2, henon2,
+eom_logistic2, logistic2,
+eom_logistic3, logistic3,
 eom_var1, var1
 
+
+export
+####################
+# Continuous systems
+###################
+eom_rossler_rossler, rossler_rossler
+
 # Initialise all the systems once, generating a trajectory.
+trajectory(anishchenko1(), 10)
 trajectory(henon2(), 10)
 trajectory(logistic2(), 10)
 trajectory(logistic3(), 10)
 trajectory(var1(), 10)
 
-####################
-# Continuous systems
-###################
-eom_rossler_rossler
-
-# Initialise all the systems once, generating a trajectory.
-trajectory(eom_rossler_rossler(), 10)
+trajectory(rossler_rossler(), 10)
 
 
 
