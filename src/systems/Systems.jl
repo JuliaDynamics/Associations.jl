e0e062965f06265a45cf42d99edc00869ef073ad
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-08-23 21:19:53 +0200

    Add Systems submodule
    Imports everything necessary to define systems.

---
+++ src/systems/Systems.jl
@@ -0,0 +1,13 @@
+module Systems
+
+using Distributions
+using DynamicalSystems
+import DynamicalSystemsBase:
+    DynamicalSystem,
+    ContinuousDynamicalSystem,
+    DiscreteDynamicalSystem,
+    Dataset
+
+import DifferentialEquations: @ode_def
+
+end
