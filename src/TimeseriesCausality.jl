4f39bd433b7ac3f4094aaeda2f3dbaffc6a87e8d
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-08-23 20:28:18 +0200

    Package basics.

---
+++ src/TimeseriesCausality.jl
@@ -0,0 +1,17 @@
+module TimeseriesCausality
+
+using DynamicalSystems
+using Documenter
+import DynamicalSystemsBase:
+    DynamicalSystem,
+    ContinuousDynamicalSystem,
+    DiscreteDynamicalSystem,
+    Dataset,
+    trajectory
+
+include("systems/Systems.jl")
+include("systems/orbit.jl")
+
+export orbit
+
+end # module
