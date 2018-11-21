6f804f689a87b44fd9261f518ecc9e0d0f28db6d
Kristian Agasøster Haaga <kahaaga@gmail.com>
2018-11-21 03:17:07 +0100

    Fixes for TE wrapper

--- src/method_wrappers/transferentropy.jl
+++ src/method_wrappers/transferentropy.jl
@@ -1,12 +1,11 @@
-using Distances
-using StateSpaceReconstruction: Embeddings
 """
     te(driver, response;
         estimator = :tetogrid,
         E = nothing, v = nothing,
         ϵ = nothing, n_ϵ = 5, ν = 1, τ = 1, dim = 3,
-        k1 = 2, k2 = 3, metric = Chebyshev(),
-        which_is_surr = :none, surr_type = aaft,
+        k1 = 2, k2 = 3, distance_metric = Chebyshev(),
+        which_is_surr = :none,
+        surr_func = aaft,
         min_numbins = nothing,
         max_numbins = nothing)
 
@@ -46,7 +45,8 @@ then TE is estimated for that bin size only.
     based estimator that computes transfer entropy as the sum of two mutual
     information terms. `k1` and `k2` sets the
     number of nearest neighbors used to estimate the mutual information terms.
-    `metric` sets the distance metric (must be valid metric from `Distances.jl`).
+    `distance_metric` sets the distance metric (must be an instance
+    of a valid metric from `Distances.jl`).
 
 ## Surrogate analysis
 A surrogate analysis will be run if `which_is_surr` is set to
@@ -59,12 +59,16 @@ The type of surrogate must be either `randomshuffle`, `randomphases`,
 `randomamplitudes`, `aaft` or `iaaft`.
 
 """
-function te(driver, response;
+function transferentropy(driver, response;
+        dim = 3,
+        τ = 1,
+        ν = 1,
         estimator = :tetogrid,
         E = nothing, v = nothing,
-        ϵ = nothing, n_ϵ = 5, ν = 1, τ = 1, dim = 3,
-        k1 = 2, k2 = 3, metric = Chebyshev(),
-        which_is_surr = :none, surr_type = aaft,
+        ϵ = nothing, n_ϵ = 5,
+        k1 = 2, k2 = 3, distance_metric = Chebyshev(),
+        which_is_surr = :none,
+        surr_func = aaft,
         min_numbins = nothing,
         max_numbins = nothing)
 
@@ -81,23 +85,23 @@ function te(driver, response;
         error("Transfer entropy method $estimator not valid.")
     end
 
-    valid_surr_types = [randomshuffle, randomphases, randomamplitudes,
-                        aaft, iaaft]
+    valid_surr_funcs = [TimeseriesSurrogates.randomshuffle,
+                        TimeseriesSurrogates.randomphases,
+                        TimeseriesSurrogates.randomamplitudes,
+                        TimeseriesSurrogates.aaft,
+                        TimeseriesSurrogates.iaaft]
 
-    if !(surr_type ∈ valid_surr_types)
-        error("Surrogate type $surr_type is not valid.")
+    if !(surr_func ∈ valid_surr_funcs)
+        error("Surrogate type $surr_func is not valid.")
     end
-
     if which_is_surr == :driver
-        driver[:] = surr_type(driver)
+        driver[:] = surr_func(driver)
     elseif which_is_surr == :response
-        response[:] = surr_type(response)
+        response[:] = surr_func(response)
     elseif which_is_surr == :both
-        driver[:] = surr_type(driver)
-        response[:] = surr_type(response)
-    elseif which_is_surr >= 3
-        error("which_is_surr $which_is_surr is not valid.")
-    else
+        driver[:] = surr_func(driver)
+        response[:] = surr_func(response)
+    elseif !(which_is_surr == :none)
         error("$which_is_surr is not a valid surrogate choice." )
     end
 
@@ -126,7 +130,7 @@ function te(driver, response;
     end
 
     if ϵ == nothing
-        ϵs = zeros(Float64, n_ϵ, dim)
+        ϵ = zeros(Float64, n_ϵ, dim)
         # We a range of bin sizes along each axis.
         for d in 1:dim
             dmin = Base.minimum(E.points[d, :])
@@ -143,232 +147,53 @@ function te(driver, response;
             max_bin_size = maxrange/max_numbins
             min_bin_size = maxrange/min_numbins
 
-            ϵs[:, d] = LinRange(log(10, min_bin_size), log(10, max_bin_size), n_ϵ)
+            ϵ[:, d] = LinRange(log(10, min_bin_size), log(10, max_bin_size), n_ϵ)
         end
+        binsizes = [ϵ[i, :] for i = 1:n_ϵ]
+        ϵs = binsizes
     else
         if length(ϵ) == 1
             ϵs = [ϵ]
+        else
+            ϵs = ϵ
         end
     end
 
     tes = zeros(Float64, length(ϵs))
+
+    # Loop over the bin sizes
     for i = 1:length(ϵs)
         if estimator ∈ (:transferentropy_transferoperator_grid,
                         :tetogrid)
-            tes[i] = transferentropy_transferoperator_grid(E, ϵs[i, :], v)
+            tes[i] = transferentropy_transferoperator_grid(E, ϵs[i], v)
 
         elseif estimator ∈ (:transferentropy_kraskov,
                             :tekraskov,
                             :tekNN)
-            tes[i] = transferentropy_kraskov(E, k1, k2, v, metric = metric)
+            tes[i] = transferentropy_kraskov(E, k1, k2, v, metric = distance_metric)
 
         elseif estimator ∈ (:transferentropy_visitfreq,
                             :tefreq)
-            tes[i] = transferentropy_visitfreq(E, ϵs[i, :], v)
+            tes[i] = transferentropy_visitfreq(E, ϵs[i], v)
 
         elseif estimator == :transferoperator_triang
-            warn("$estimator is not implemented yet.")
-            #warn("Invariantizing embedding before calculating transfer matrix...")
-            #t = triangulate(invariantize(E))
-            #TO = transferoperator_approx(t, n_pts = n_randpts, sample_randomly = sample_randomly)
-            #invdist = left_eigenvector(TO)
-            #tes[i] = transferentropy_transferoperator_triang(t, invdist, n_ϵ, n_reps)
+                warn("$estimator is not implemented yet.")
+                #warn("Invariantizing embedding before calculating transfer matrix...")
+                #t = triangulate(invariantize(E))
+                #TO = transferoperator_approx(t, n_pts = n_randpts, sample_randomly = sample_randomly)
+                #invdist = left_eigenvector(TO)
+                #tes[i] = transferentropy_transferoperator_triang(t, invdist, n_ϵ, n_reps)
         end
+
     end
+
     # If we only use one binsize, we can't take the integral. This is always
     # the case for the kNN based estimators, because they don't use a bin size.
-    if !(ϵ == nothing) || estimator ∈ estims_te_kNN
+    if estimator ∈ estims_te_kNN
         return tes[1]
     else
-        mean(tes)
+        return mean(tes)
     end
 end
-
-# """
-#     te_lags(driver, response, lags::Union{Vector{Int}, UnitRange{Int}};
-#         method = :transferoperator_grid,
-#         E = nothing, v = nothing,
-#         ϵ = nothing, n_ϵ = 5, dim = 3, η = 1,
-#         k1 = 4, k2 = 5, metric = Chebyshev(),
-#         which_is_surr = -1, surr_type = aaft,
-#         max_numbins = nothing, min_numbins = nothing)
-#
-# Compute transfer entropy from a `driver` time series to a `response` time
-# series over a range of forward prediction `lags`.
-#
-# ## Embedding instructions
-# Specify embedding and instructions for how to compute marginal
-# entropies are with `E::AbstractEmbedding` and `v::TEVars`. If either
-# is missing, the data is embedded using the provided forward lag `ν`, embedding
-# dimension `dim` and backward lag `η`.
-#
-# `method` sets the transfer entropy algorithm to use. There are two types of
-# estimators: grid-based approaches, and nearest-neighbor based approaches.
-#
-# ## Grid-based estimators
-#
-# - `:transferoperator_grid`. Grid-based transfer entropy estimator based on the
-# transfer operator.
-# - `:visitfreq`. Simple visitation frequency based estimator.
-#
-# For all grid based algorithms, `ϵ` sets the binsize. If `ϵ = nothing`, then the
-# algorithm uses a bin size corresponding to a number of subdivisions `N`
-# along each axis so that `N ≦ npoints^(1/(dim+1))`. Transfer entropy is then
-# computed as an average over `n_ϵ` different bin sizes corresponding to
-# `N-1` to `N` subdivisions along each axis.
-#
-# ## Nearest neighbor based estimator.
-# - `:kraskov`: A nearest-neighbor based estimator, which computes transfer
-# entropy as the sum of two mutual information terms. `k1` and `k2` sets the
-# number of nearest neighbors for those terms. `metric` sets the distance
-# metric (must be valid metric from `Distances.jl`).
-#
-# ## Surrogate analysis
-# A surrogate analysis will be run if `which_is_surr` is set to 0, 1 or 2.
-# - `which_is_surr = 0` will construct a surrogate for both time series.
-# - `which_is_surr = 1` will construct a surrogate for the driver time series.
-# - `which_is_surr = 2` will construct a surrogate for the response time series.
-# """
-# te_lags(driver, response, lags::Union{Vector{Int}, UnitRange{Int}};
-#        method = :transferoperator_grid,
-#        E = nothing, v = nothing,
-#        ϵ = nothing, n_ϵ = 5, dim = 3, η = 1,
-#        k1 = 4, k2 = 5, metric = Chebyshev(),
-#        which_is_surr = -1, surr_type = aaft,
-#        max_numbins = nothing, min_numbins = nothing)
-#
-#     tes = zeros(Float64, length(lags))
-#     for i in 1:length(lags)
-#         tes[i] = te(driver, response,
-#                     lag = lags[i],
-#                     method = method,
-#                     E = E, v = v,
-#                     ϵ = ϵ, n_ϵ = n_ϵ, dim = dim, η = η,
-#                     k1 = k1, k2 = k2, metric = metric,
-#                     which_is_surr = which_is_surr,
-#                     surr_type = surr_type)
-#     end
-#
-#     return tes
-#
-#     # pmap(l -> te(driver, response,
-#     #                lag = l,
-#     #                method = method,
-#     #                ϵ = ϵ, n_ϵ = n_ϵ, dim = dim, η = η,
-#     #                k1 = k1, k2 = k2, metric = metric,
-#     #                which_is_surr = which_is_surr,
-#     #                surr_type = surr_type,
-#     #                min_numbins = min_numbins,
-#     #                max_numbins = max_numbins), lags)
-# end
-#
-# """
-#     te(E::AbstractEmbedding, v::TEVars;
-#         method = :transferoperator_grid,
-#         ϵ = nothing, n_ϵ = 5, lag = 1, η = 1,
-#         k1 = 2, k2 = 3, metric = Chebyshev(),
-#         which_is_surr = -1, surr_type = aaft,
-#         min_numbins = nothing,
-#         max_numbins = nothing)
-# """
-# function te(E::AbstractEmbedding, v::TEVars;
-#         method = :transferoperator_grid,
-#         ϵ = nothing, n_ϵ = 5, lag = 1, η = 1,
-#         k1 = 2, k2 = 3, metric = Chebyshev(),
-#         which_is_surr = -1, surr_type = aaft,
-#         min_numbins = nothing,
-#         max_numbins = nothing)
-#
-#     valid_methods = [:transferoperator_grid,
-#                     :kraskov,
-#                     :visitfreq]
-#
-#     dim = dimension(E)
-#     if !(method ∈ valid_methods)
-#         error("Transfer entropy method $method not valid.")
-#     end
-#
-#     if !(ϵ == nothing)
-#         ϵs = [ϵ for i = 1:dim]
-#     else
-#         ϵs = zeros(Float64, n_ϵ, dim)
-#         # We a range of bin sizes along each axis.
-#         for d in 1:dim
-#             dmin = minimum(E.points[d, :])
-#             dmax = maximum(E.points[d, :])
-#             maxrange = dmax - dmin
-#
-#             # If minimum and maximum number of bins are not provided,
-#             # use the Palus recommendation of N^(1/dim) as a max.
-#             if !(typeof(min_numbins) == Int && typeof(max_numbins) == Int)
-#                 max_numbins = ceil(Int, npoints(E)^(1/(dim+1)))
-#                 min_numbins = max_numbins - 1
-#             end
-#
-#             max_bin_size = maxrange/max_numbins
-#             min_bin_size = maxrange/min_numbins
-#
-#             ϵs[:, d] = LinRange(log(10, min_bin_size), log(10, max_bin_size), n_ϵ)
-#         end
-#     end
-#
-#     tes = zeros(Float64, n_ϵ)
-#     for i = 1:n_ϵ
-#         if method == :transferoperator_grid
-#             tes[i] = transferentropy_transferoperator_visitfreq(E, ϵs[i, :], v)
-#
-#         elseif method == :kraskov
-#             tes[i] = transferentropy_kraskov(E, k1, k2, v, metric = metric)
-#
-#         elseif method == :visitfreq
-#             tes[i] = transferentropy_visitfreq(E, ϵs[i, :], v)
-#
-#         elseif method == :transferoperator_triang
-#             warn("Invariantizing embedding before calculating transfer matrix...")
-#             t = triangulate(invariantize(E))
-#             TO = transferoperator_approx(t, n_pts = n_randpts, sample_randomly = sample_randomly)
-#             invdist = left_eigenvector(TO)
-#             tes[i] = transferentropy_transferoperator_triang(t, invdist, n_ϵ, n_reps)
-#         end
-#     end
-#
-#     # If we only use one binsize, we can't take the integral.
-#     if !(ϵ == nothing) || method == :kraskov
-#         return tes[1]
-#     else
-#         # there's a problem with Cubature when run in parallel, so stick with
-#         # mean for now
-#         mean(tes)#∫te(1:n_ϵ, tes)
-#     end
-# end
-#
-# """
-#     te_lags(E::AbstractEmbedding, v::TEVars, lags::Union{Vector{Int},
-#         UnitRange{Int}}; method = :transferoperator_grid,
-#         ϵ = nothing, n_ϵ = 5, η = 1,
-#         k1 = 4, k2 = 5, metric = Chebyshev(),
-#         which_is_surr = -1, surr_type = aaft,
-#         max_numbins = nothing, min_numbins = nothing)
-#
-# """
-# function te_lags(E::AbstractEmbedding, v::TEVars, lags::Union{Vector{Int},
-#         UnitRange{Int}}; method = :transferoperator_grid,
-#         ϵ = nothing, n_ϵ = 5, η = 1,
-#         k1 = 4, k2 = 5, metric = Chebyshev(),
-#         which_is_surr = -1, surr_type = aaft,
-#         max_numbins = nothing, min_numbins = nothing)
-#
-#     pmap(l -> te(E, v,
-#                 lag = l,
-#                 method = method,
-#                 ϵ = ϵ, n_ϵ = n_ϵ, η = η,
-#                 k1 = k1, k2 = k2, metric = metric,
-#                 which_is_surr = which_is_surr,
-#                 surr_type = surr_type,
-#                 min_numbins = min_numbins,
-#                 max_numbins = max_numbins), lags)
-# end
-#
-# transferentropy = te
-
-export te, te_lags
+te = transferentropy
+export transferentropy, te
