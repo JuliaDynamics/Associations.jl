1a87177f2252ee9fd4a2a6489dc0a6cf9bfec589
Kristian Agasøster Haaga <kahaaga@gmail.com>
2019-09-11 09:43:40 +0200

    Add examples to `causality` function

--- src/causalitytests/causalitytests.jl
+++ src/causalitytests/causalitytests.jl
@@ -1,5 +1,10 @@
 import CausalityToolsBase: CausalityTest
-
+import UncertainData
+import UncertainData: 
+    AbstractUncertainValue, 
+    AbstractUncertainValueDataset, 
+    AbstractUncertainIndexValueDataset,
+    resample
 
 """
     DistanceBasedCausalityTest
@@ -36,11 +41,35 @@ applying the causality estimator specified by `params`.
 directionality(source, target, params::CausalityTest)
 
 """
-    directionality(source, target, params::CausalityTest)
     causality(source, target, params::CausalityTest)
 
 Test whether there is a dynamical influence from `source` to `target` by 
-applying the causality estimator specified by `params`. 
+applying the causality estimator specified by `params`, which must be 
+a valid causality test.
+
+## Examples 
+
+```julia
+x, y = rand(500), rand(500)
+
+# Test for a causal relation between `x` and `y` using a cross mapping test 
+# with default parameters
+causality(x, y, CrossMappingTest())
+          
+# Test for a causal relation between `x` and `y` using a transfer entropy test 
+# with default parameters over a rectangular binning with five equidistant 
+# intervals along each axis and prediction lag 1
+binning =  RectangularBinning(5)
+causality(x, y, TransferOperatorGrid(binning = binning, ηs = 1))
+
+# Test for a causal relation between `x` and `y` using a predictive asymmetry 
+# test with a transfer entropy test as the underlying test, using 
+# default parameters over a rectangular binning with ten equidistant 
+# intervals along each axis and prediction lags -10 to 10
+binning =  RectangularBinning(10)
+te_test = TransferOperatorGridTest(binning = binning, ηs = -10:10)
+causality(x, y, PredictiveAsymmetryTest(predictive_test = te_test))
+```
 """
 causality(source, target, params::CausalityTest)
 
@@ -54,6 +83,10 @@ end
 
 Base.show(io::IO, test::CausalityTest) = print(io, summarise(test))
 
+# Resample vectors
+
+UncertainData.resample(v) = v
+UncertainData.resample(v::Vector{T}) where T = v
 
 include("distance_based_tests/JointDistanceDistributionTest.jl")
 include("distance_based_tests/CrossMappingTest.jl")
@@ -67,4 +100,5 @@ export
     causality,
     DistanceBasedCausalityTest,
     EntropyBasedCausalityTest,
-    TransferEntropyTest
+    TransferEntropyTest,
+    resample
