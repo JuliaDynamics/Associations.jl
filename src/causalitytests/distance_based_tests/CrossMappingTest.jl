ad8f563a63255c1c4fad5437fe87b18f43fb03fb
Kristian Agasøster Haaga <kahaaga@gmail.com>
2019-09-12 02:08:00 +0200

    Integrate CrossMappingTest with UncertainData

--- src/causalitytests/distance_based_tests/CrossMappingTest.jl
+++ src/causalitytests/distance_based_tests/CrossMappingTest.jl
@@ -186,4 +186,39 @@ function causality(source, target, p::CrossMappingTest)
         correspondence_measure = p.correspondence_measure)
 end
 
+################################################################
+# Integration with UncertainData.jl
+################################################################
+function causality(source::Vector{<:UVT1}, target::Vector{<:UVT2}, p::CrossMappingTest) where {
+        UVT1<:AbstractUncertainValue, 
+        UVT2<:AbstractUncertainValue}
+    causality(resample.(source), resample.(target), p)
+end
+
+function causality(source, target::Vector{<:UVT}, p::CrossMappingTest) where { 
+        UVT<:AbstractUncertainValue}
+    causality(source, resample.(target), p)
+end
+
+function causality(source::Vector{<:UVT}, target, p::CrossMappingTest) where {
+        UVT<:AbstractUncertainValue}
+    causality(resample.(source), target, p)
+end
+
+function causality(source::UVT1, target::UVT2, p::CrossMappingTest) where {
+        UVT1<:AbstractUncertainValueDataset, 
+        UVT2<:AbstractUncertainValueDataset}
+    causality(resample.(source), resample.(target), p)
+end
+
+function causality(source, target::UVT, p::CrossMappingTest) where { 
+        UVT<:AbstractUncertainValueDataset}
+    causality(source, resample.(target), p)
+end
+
+function causality(source::UVT, target, p::CrossMappingTest) where {
+        UVT<:AbstractUncertainValueDataset}
+    causality(resample.(source), target, p)
+end
+
 export CrossMappingTest
--- test/causality_tests/test_CrossMappingTest.jl
+++ test/causality_tests/test_CrossMappingTest.jl
@@ -1,2 +1,38 @@
+# Vectors of uncertain values
+uvals_x = [UncertainValue(Normal, rand(Normal(0, 5)), abs(rand(Normal(0, 3)))) for i = 1:100]
+uvals_y = [UncertainValue(Normal, rand(Normal(0, 5)), abs(rand(Normal(0, 3)))) for i = 1:100];
 
-causality(rand(300), rand(300), CrossMappingTest(n_reps = 100))
+# UncertainValueDataset
+UVX = UncertainValueDataset(uvals_x)
+UVY = UncertainValueDataset(uvals_y)
+
+# UncertainIndexDataset
+UVX_idx = UncertainIndexDataset(uvals_x)
+UVY_idx = UncertainIndexDataset(uvals_y)
+
+# Real-valued vectors
+x = resample.(uvals_x);
+y = resample.(uvals_y);
+
+################################################################
+# Integration with UncertainData.jl
+################################################################
+ctest = CrossMappingTest(n_reps = 10)
+
+# We should get a vector of cross map values
+@test causality(x, y, ctest) isa Vector{T} where T
+@test causality(uvals_x, uvals_y, ctest) isa Vector{T} where T
+@test causality(x, uvals_y, ctest) isa Vector{T} where T
+@test causality(uvals_x, y, ctest) isa Vector{T} where T
+@test causality(UVX, UVY, ctest) isa Vector{T} where T
+@test causality(x, UVY, ctest) isa Vector{T} where T
+@test causality(UVX, y, ctest) isa Vector{T} where T
+
+# n_reps = 10, so we should get 10 cross map values
+@test causality(x, y, ctest) |> length == 10
+@test causality(uvals_x, uvals_y, ctest) |> length == 10
+@test causality(x, uvals_y, ctest) |> length == 10
+@test causality(uvals_x, y, ctest) |> length == 10
+@test causality(UVX, UVY, ctest) |> length == 10
+@test causality(x, UVY, ctest) |> length == 10
+@test causality(UVX, y, ctest) |> length == 10
