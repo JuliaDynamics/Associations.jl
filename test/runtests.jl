7700c79df04a6c376cc6de482f8af0f086c8d8b7
Kristian Agasøster Haaga <kahaaga@gmail.com>
2019-11-13 23:08:19 +0100

    Activate tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
-# if lowercase(get(ENV, "CI", "false")) == "true"
-#     include("install_dependencies.jl")
-# end
+if lowercase(get(ENV, "CI", "false")) == "true"
+    include("install_dependencies.jl")
+end
 
 using Test
 using CausalityTools
@@ -11,36 +11,36 @@ using UncertainData
 # 	include("plot_recipes.jl")
 # end
 
-# @testset "Discrete example systems" begin 
-#     include("systems/discrete/test_discrete_systems.jl")
-# end
+@testset "Discrete example systems" begin 
+    include("systems/discrete/test_discrete_systems.jl")
+end
 
-# @testset "Continuous example systems" begin 
-#     include("systems/continuous/test_continuous_systems.jl")
-# end
+@testset "Continuous example systems" begin 
+    include("systems/continuous/test_continuous_systems.jl")
+end
 
-# @testset "High level wrappers" begin
-#     include("test_wrappers_te.jl")
-# end
+@testset "High level wrappers" begin
+    include("test_wrappers_te.jl")
+end
 
-# @testset "Causality tests on scalar time series" begin
-#     include("causality_tests/test_scalarseries_CrossMappingTest.jl")
-#     include("causality_tests/test_scalarseries_ConvergentCrossMappingTest.jl")
-#     include("causality_tests/test_scalarseries_JointDistanceDistributionTest.jl")
-#     include("causality_tests/test_scalarseries_JointDistanceDistributionTTest.jl")
-#     include("causality_tests/test_scalarseries_TransferOperatorGridTest.jl")
-#     include("causality_tests/test_scalarseries_VisitationFrequencyTest.jl")
-#     include("causality_tests/test_scalarseries_ApproximateSimplexIntersectionTest.jl")
-#     include("causality_tests/test_scalarseries_ExactSimplexIntersectionTest.jl")
-#     include("causality_tests/test_scalarseries_PredictiveAsymmetryTest.jl")
-#     include("causality_tests/test_scalarseries_SMeasure.jl")
-# end
+@testset "Causality tests on scalar time series" begin
+    include("causality_tests/test_scalarseries_CrossMappingTest.jl")
+    include("causality_tests/test_scalarseries_ConvergentCrossMappingTest.jl")
+    include("causality_tests/test_scalarseries_JointDistanceDistributionTest.jl")
+    include("causality_tests/test_scalarseries_JointDistanceDistributionTTest.jl")
+    include("causality_tests/test_scalarseries_TransferOperatorGridTest.jl")
+    include("causality_tests/test_scalarseries_VisitationFrequencyTest.jl")
+    include("causality_tests/test_scalarseries_ApproximateSimplexIntersectionTest.jl")
+    include("causality_tests/test_scalarseries_ExactSimplexIntersectionTest.jl")
+    include("causality_tests/test_scalarseries_PredictiveAsymmetryTest.jl")
+    include("causality_tests/test_scalarseries_SMeasure.jl")
+end
 
 
-# @testset "DynamicalSystems.jl integration" begin
-#     include("causality_tests/integration_dynamicalsystems/test_integration_discrete_system.jl")
-#     include("causality_tests/integration_dynamicalsystems/test_integration_continuous_system.jl")
-# end
+@testset "DynamicalSystems.jl integration" begin
+    include("causality_tests/integration_dynamicalsystems/test_integration_discrete_system.jl")
+    include("causality_tests/integration_dynamicalsystems/test_integration_continuous_system.jl")
+end
 
 @testset "UncertainData.jl integration" begin
     include("causality_tests/integration_uncertaindata/test_uncertaindata_integration_JointDistanceDistributionTest.jl")
