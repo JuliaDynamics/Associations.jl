bb424dcf3ac8f5d29f42eb66ec8c3f5d010feaf6
Kristian Agasøster Haaga <kahaaga@gmail.com>
2019-12-23 21:22:06 +0100

    Update message

--- src/CausalityTools.jl
+++ src/CausalityTools.jl
@@ -14,9 +14,8 @@ module CausalityTools
     @reexport using TransferEntropy
     @reexport using CrossMappings
 
-
     display_update = true
-    version = "v0.8.0"
+    version = "v0.9.0"
     update_name = "update_$version"
 
     if display_update
@@ -25,51 +24,35 @@ module CausalityTools
     """
     \nUpdate message: CausalityTools $version
     ----------------
-    BREAKING CHANGES! 
+    New functionality  
     ----------------
-    
+    #### TransferEntropy.jl
+
+    - Added [`NearestNeighbourMI`](@ref) transfer entropy estimator.
+    - Added [`transferentropy(::Any, ::TEVars, ::NearestNeighbourMI)`](@ref) method.
     - Transfer entropy estimators now contain a field `b` which gives the base of the logarithm
         used during transfer entropy computations, and hence dictates the unit of the transfer 
         entropy. By default, `b = 2`, which gives the transfer entropy in bits. The keyword `b` 
         is thus obsolete in all transfer entropy methods that used it before.
 
-    - The `ν` keyword for the `ConvergentCrossMappingTest` and `CrossMappingTest` has changed to 
-        `η`, to conform with the convention for the transfer entropy tests.
-    
-    There are also some breaking changes to the transfer operator and invariant measure 
-    estimators in this release. Other new features:
+    #### CausalityTools.jl
 
-    - Common interface for causality testing with the `causality` function and its methods.
-    - Integration with UncertainData.jl. `causality` accepts uncertain datasets as inputs.
-    - Integration with DynamicalSystems.jl. `causality` accepts dynamical systems as inputs.
+    - Added [`NearestNeighbourMITest`](@ref) transfer entropy test.
+    - Added [`NormalisedPredictiveAsymmetryTest`](@ref), for which the predictive asymmetry 
+        is normalised to some fraction of the mean of the raw values of the predictive statistic.
+    - All subtypes of `CausalityTest` are now mutable. This allows adjusting test 
+        parameters during sensitivity analyses without creating a new test every time.
 
-    Check the online documentation for more information!
+    #### TimeseriesSurrogates.jl
 
-    SYNTAX CHANGES
-    --------------
-      
-    Valid inputs
-    ------------
+    - Plotting functionality is behind a Requires-block. To use the plotting functionality, you 
+        now need to do `using Plots` beforehand.
 
-    The inputs to the following listed functions should be either a `Dataset` instance, 
-    a `CustomReconstruction` instance, or a vector of `Vector`, `SVector` or `MVector`. 
+    ### Documentation
 
-    Invariant measures: 
-
-    - `invariantmeasure(pts, RectangularBinning(ϵ)` where `ϵ` indicates the type of binning.
-    - `invariantmeasure(pts, RectangularBinning(ϵ)` where `ϵ` indicates the type of binning.
-
-    Transfer operators:
-
-    - `transferoperator(pts, RectangularBinning(ϵ)` for transfer operators over rectangular 
-        binnings.
-    - `transferoperator(pts, TriangulationBinning(), ExactIntersection())` for transfer 
-        operators over a triangulation of `pts` using exact simplex intersections to compute 
-        transition probabilities.
-    - `transferoperator(pts, TriangulationBinning(), ApproxIntersection())` for transfer 
-    operators over a triangulation of `pts` using approximate simplex intersections to compute 
-    transition probabilities.
-
+    - Fixed small error in documentation for `DiscreteSystemSetup`.
+    - Created high-level pages for transfer entropy. Re-did documentation.
+    - Created high-level pages for cross mappings.
     """; color = :light_magenta)
     touch(joinpath(@__DIR__, update_name))
     end
