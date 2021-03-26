3c453bd6db3d753769abdafb5739abfda74d27a8
Kristian Agasøster Haaga <kahaaga@gmail.com>
2021-03-26 17:54:31 +0100

    Simplify methods signatures for UncertainData integration

--- src/uncertaindata.jl
+++ src/uncertaindata.jl
@@ -1,87 +1,86 @@
 
-import UncertainData: resample 
-
+import UncertainData: resample, UncertainDataset, UncertainIndexDataset, UncertainValueDataset, UncertainIndexValueDataset
 
 ##################################################
 # Basic resampling for `UncertainDataset`s
 ##################################################
 UT = Union{UncertainValueDataset, UncertainIndexDataset, UncertainDataset}
 
-s_measure(source::UT, target::UT, m::Int, τ, K::Int; kwargs...) =
-    s_measure(resample(source), resample(target), m, τ, K; kwargs...)
+s_measure(s::UT, t::UT, args...; kwargs...) =
+    s_measure(resample(s), resample(t), args...; kwargs...)
 
-jdd(source::UT, target::UT, m::Int, τ, K::Int; kwargs...) =
-    jdd(resample(source), resample(target), m, τ, K; kwargs...)
+jdd(s::UT, t::UT, m::Int, args...; kwargs...) =
+    jdd(resample(s), resample(t), args...; kwargs...)
 
-mutualinfo(source::UT, target::UT, estimator; kwargs...) =
-    mutualinfo(resample(source), resample(target), estimator; kwargs...)
+mutualinfo(s::UT, t::UT, args...; kwargs...) =
+    mutualinfo(resample(s), resample(t), args...; kwargs...)
 
-transferentropy(source::UT, target::UT, estimator; kwargs...) =
-    transferentropy(resample(source), resample(target), estimator; kwargs...)
+transferentropy(s::UT, t::UT, args...; kwargs...) =
+    transferentropy(resample(s), resample(t), args...; kwargs...)
 
-transferentropy(source::UT, target::UT, cond::UT, estimator; kwargs...) =
-    transferentropy(resample(source), resample(target), resample(cond), estimator; kwargs...)
+transferentropy(s::UT, t::UT, c::UT, args...; kwargs...) =
+    transferentropy(resample(s), resample(t), resample(c), args...; kwargs...)
 
-predictive_asymmetry(source::UT, target::UT, estimator; kwargs...) =
-    predictive_asymmetry(resample(source), resample(target), estimator; kwargs...)
+predictive_asymmetry(s::UT, t::UT, args...; kwargs...) =
+    predictive_asymmetry(resample(s), resample(t), args...; kwargs...)
 
-predictive_asymmetry(source::UT, target::UT, cond::UT, estimator; kwargs...) =
-    predictive_asymmetry(resample(source), resample(target), resample(cond), estimator; kwargs...)
+predictive_asymmetry(s::UT, t::UT, c::UT, args...; kwargs...) =
+    predictive_asymmetry(resample(s), resample(t), resample(c), args...; kwargs...)
 
-crossmap(source::UT, target::UT; kwargs...) = 
-    crossmap(resample(source), resample(target); kwargs...)
+crossmap(s::UT, t::UT, args...; kwargs...) = 
+    crossmap(resample(s), resample(t), args...; kwargs...)
 
-ccm(source::UT, target::UT, timeseries_lengths; kwargs...) = 
-    ccm(resample(source), resample(target), timeseries_lengths; kwargs...)
+ccm(s::UT, t::UT, args...; kwargs...) = 
+    ccm(resample(s), resample(t), args...; kwargs...)
 
 ##########################################################################
 # Basic resampling for `UncertainIndexValueDataset` (no constraints)
 ##########################################################################
-UIVD = UncertainIndexValueDataset
+const UIVD = UncertainIndexValueDataset
 
 # TODO: warn about potential index reversals?
 #
-# function warn_about_sampling(source::V, target::W) 
-#     if source isa UIVD
-#         @warn "`source` isa UncertainIndexValueDataset. Index reversals may occur. Consider constrained resampling."
+# function warn_about_sampling(s::V, t::W) 
+#     if s isa UIVD
+#         @warn "`s` isa UncertainIndexValueDataset. Index reversals may occur. Consider constrained resampling."
 #     end
 
-#     if target isa UIVD
-#         @warn "`target` isa UncertainIndexValueDataset. Index reversals may occur. Consider constrained resampling."
-#     end
-# end
-
-# function warn_about_sampling(source::V, target::W, cond::X)
-#     warn_about_sampling(source, target)
-#     if cond isa UIVD
-#         @warn "`cond` isa UncertainIndexValueDataset. Index reversals may occur. Consider constrained resampling."
+#     if t isa UIVD
+#         @warn "`t` isa UncertainIndexValueDataset. Index reversals may occur. Consider constrained resampling."
 #     end
 # end
 
-s_measure(source::UIVD, target::UIVD, m::Int, τ, K::Int; kwargs...)
-    s_measure(resample(source.values), resample(target.values), m, τ, K; kwargs...)
+# function warn_about_sampling(s::V, t::W, c::X)
+#     warn_about_sampling(s, t)
+#     if c isa UIVD
+#         @warn "`c` isa UncertainIndexValueDataset. Index reversals may occur. Consider constrained resampling."
+#     end
+# end
 
-jdd(source::UIVD, target::UIVD, m::Int, τ, K::Int; kwargs...) =
-    jdd(resample(source.values), resample(target.values), m, τ, K; kwargs...)
+s_measure(s::UIVD, t::UIVD, args...; kwargs...)
+    s_measure(resample(s.values), resample(t.values), args...; kwargs...)
 
-mutualinfo(source::UIVD, target::UIVD, estimator; kwargs...) =
-    mutualinfo(resample(source.values), resample(target.values), estimator; kwargs...)
+jdd(s::UIVD, t::UIVD, args...; kwargs...) =
+    jdd(resample(s.values), resample(t.values), args...; kwargs...)
 
-transferentropy(source::UIVD, target::UIVD, estimator; kwargs...) =
-    transferentropy(resample(source.values), resample(target.values), estimator; kwargs...)
+mutualinfo(s::UIVD, t::UIVD, args...; kwargs...) =
+    mutualinfo(resample(s.values), resample(t.values), args...; kwargs...)
 
-transferentropy(source::UIVD, target::UIVD, cond::UIVD, estimator; kwargs...) =
-    transferentropy(resample(source.values), resample(target.values), resample(cond.values), estimator; kwargs...)
+transferentropy(s::UIVD, t::UIVD, args...; kwargs...) =
+    transferentropy(resample(s.values), resample(t.values), args...; kwargs...)
 
-predictive_asymmetry(source::UIVD, target::UIVD, estimator; kwargs...) =
-    predictive_asymmetry(resample(source.values), resample(target.values), estimator; kwargs...)
+transferentropy(s::UIVD, t::UIVD, c::UIVD, args...; kwargs...) =
+    transferentropy(resample(s.values), resample(t.values), resample(c.values), args...; kwargs...)
 
-predictive_asymmetry(source::UIVD, target::UIVD, cond::UIVD, estimator; kwargs...) =
-    predictive_asymmetry(resample(source.values), resample(target.values), resample(cond.values), estimator; kwargs...)
+predictive_asymmetry(s::UIVD, t::UIVD, args...; kwargs...) =
+    predictive_asymmetry(resample(s.values), resample(t.values), args...; kwargs...)
 
-crossmap(source::UIVD, target::UIVD; kwargs...) = 
-    crossmap(resample(source.values), resample(target.values); kwargs...)
+predictive_asymmetry(s::UIVD, t::UIVD, c::UIVD, args...; kwargs...) =
+    predictive_asymmetry(resample(s.values), resample(t.values), resample(c.values), args...; kwargs...)
 
-ccm(source::UIVD, target::UIVD, timeseries_lengths; kwargs...) = 
-    ccm(resample(source.values), resample(target.values), timeseries_lengths; kwargs...)
+crossmap(s::UIVD, t::UIVD, args...; kwargs...) = 
+    crossmap(resample(s.values), resample(t.values), args...; kwargs...)
+
+ccm(s::UIVD, t::UIVD, args..., kwargs...) = 
+    ccm(resample(s.values), resample(t.values), args...; kwargs...)
 
