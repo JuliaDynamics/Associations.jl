b9c41f2c6fb92d5a4347229573c60a16428f25fe
Kristian Agasøster Haaga <kahaaga@gmail.com>
2021-03-29 22:27:30 +0200

    Organize integrations

--- src/CausalityTools.jl
+++ src/CausalityTools.jl
@@ -18,7 +18,7 @@ module CausalityTools
     using Requires 
     function __init__()
         @require UncertainData="dcd9ba68-c27b-5cea-ae21-829cd07325bf" begin
-            include("uncertaindata.jl")
+            include("integrations/uncertaindata.jl")
         end
 
         #@require Simplices="d5428e67-3037-59ba-9ab1-57a04f0a3b6a" begin
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,3 +7,5 @@ include("methods/test_predictive_asymmetry.jl")
 
 include("systems/discrete/test_discrete_systems.jl")
 include("systems/continuous/test_continuous_systems.jl")
+
+include("integrations/test_uncertaindata_integration.jl")
