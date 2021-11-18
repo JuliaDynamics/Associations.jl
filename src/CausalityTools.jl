8eb5138c4d3fd7da522a74a5ffa78ba050dbfd66
Kristian Agasøster Haaga <kahaaga@gmail.com>
2021-11-18 01:03:03 +0100

    Don't export UncertainData for now

--- src/CausalityTools.jl
+++ src/CausalityTools.jl
@@ -18,9 +18,9 @@ module CausalityTools
 
     using Requires 
     function __init__()
-        @require UncertainData="dcd9ba68-c27b-5cea-ae21-829cd07325bf" begin
-            include("integrations/uncertaindata.jl")
-        end
+        #@require UncertainData="dcd9ba68-c27b-5cea-ae21-829cd07325bf" begin
+        #   include("integrations/uncertaindata.jl")
+        #end
 
         #@require Simplices="d5428e67-3037-59ba-9ab1-57a04f0a3b6a" begin
         # #   import PerronFrobenius: SimplexExact, SimplexPoint
