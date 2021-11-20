894744d65c522dde34ced3b44fd4bb33cb5070ae
Kristian Agasøster Haaga <kahaaga@gmail.com>
2021-11-20 19:23:50 +0100

    Export OneSampleTTest and pvalue for jdd

--- src/JointDistanceDistribution/JointDistanceDistribution.jl
+++ src/JointDistanceDistribution/JointDistanceDistribution.jl
@@ -1,5 +1,7 @@
 using Reexport
 
 @reexport module JointDistanceDistribution
+    import HypothesisTests: OneSampleTTest, pvalue
+    export OneSampleTTest, pvalue 
     include("joint_distance_distribution.jl")
 end
