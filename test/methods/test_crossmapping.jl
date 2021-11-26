394c71e162bf2b854d90b62906693c5a254ffb35
Kristian Haaga <kahaaga@gmail.com>
2021-11-26 04:10:57 +0100

    Actually test `crossmap`

--- test/methods/test_crossmapping.jl
+++ test/methods/test_crossmapping.jl
@@ -4,9 +4,9 @@ using CausalityTools, StatsBase
 
 	@testset "Prediction lags" begin
 	    x, y = rand(100), rand(100)
-	    crossmap(x, y, 3, 1) isa Float64
-		crossmap(x, y, 3, 1, :random) isa Vector{Float64}
-		crossmap(x, y, 3, 1, :segment) isa Vector{Float64}
+	    @test crossmap(x, y, 3, 1) isa Float64
+		@test crossmap(x, y, 3, 1, :random) isa Vector{Float64}
+		@test crossmap(x, y, 3, 1, :segment) isa Vector{Float64}
 	end
 
 end
