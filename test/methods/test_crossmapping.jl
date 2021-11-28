b9ddceffde1237eb18c223f03ab59418e1097102
Kristian Agasøster Haaga <kahaaga@users.noreply.github.com>
2021-11-28 12:42:22 +0100

    Pairwise asymmetric inference (#149)
    * Add a utils function that takes care of the embedding
    
    Everything is equivalent for PAI and CCM, except for the embedding step, so separate this step
    
    * Add pairwise asymmetric inference algorithm
    
    * Add tests
    
    * Documentation example
    
    * Isolate crossmapping code, embed separately for CCM and PAI
    
    * Update docs
    
    * Give examples blocks a unique name
    
    * Add method to overview
    
    * Latex syntax
    
    * Circumvent soft-scope warning about the variable `orbit`

--- test/methods/test_crossmapping.jl
+++ test/methods/test_crossmapping.jl
@@ -2,11 +2,14 @@ using CausalityTools, StatsBase
 
 @testset "Cross mapping" begin
 
-	@testset "Prediction lags" begin
-	    x, y = rand(100), rand(100)
-	    @test crossmap(x, y, 3, 1) isa Float64
-		@test crossmap(x, y, 3, 1, :random) isa Vector{Float64}
-		@test crossmap(x, y, 3, 1, :segment) isa Vector{Float64}
-	end
+	x, y = rand(100), rand(100)
+	@test crossmap(x, y, 3, 1) isa Float64
+	@test crossmap(x, y, 3, 1, :random) isa Vector{Float64}
+	@test crossmap(x, y, 3, 1, :segment) isa Vector{Float64}
+end
 
+@testset "Pairwise asymmetric inference" begin
+	@test pai(x, y, 3, 1) isa Float64
+	@test pai(x, y, 3, 1, :random) isa Vector{Float64}
+	@test pai(x, y, 3, 1, :segment) isa Vector{Float64}
 end
