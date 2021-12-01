944816a2d14e1f4343779eadc286711698533dee
Kristian Haaga <kahaaga@gmail.com>
2021-12-01 06:40:33 +0100

    Update test

--- test/methods/test_leanings.jl
+++ test/methods/test_leanings.jl
@@ -1,11 +1,11 @@
 
-x = [0, 0, 1, 0, 0, 1, 0, 0, 1, 0]
-y = [0, 0, 0, 1, 0, 0, 1, 0, 0, 1]
-
 @testset "Leanings" begin
-    @test mean_observed_penchant(x, y, 1, weighted = false) ≈ 1.0
-    @test mean_observed_penchant(y, x, 1, weighted = false) ≈ 1/7
 
-    @test mean_observed_penchant(x, y, 1, weighted = true) ≈ 1.0
-    @test mean_observed_penchant(y, x, 1, weighted = true) ≈ 3/63
+    x = [0, 0, 1, 0, 0, 1, 0, 0, 1, 0]
+    y = [0, 0, 0, 1, 0, 0, 1, 0, 0, 1]
+    @test penchant(x, y, 1, weighted = false) ≈ 1.0
+    @test penchant(y, x, 1, weighted = false) ≈ 1/7
+
+    @test penchant(x, y, 1, weighted = true) ≈ 1.0
+    @test penchant(y, x, 1, weighted = true) ≈ 3/63
 end
