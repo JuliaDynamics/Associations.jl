208e4a5edc74403b1aa98030a1505b1d173d1ab2
Kristian Haaga <kahaaga@gmail.com>
2021-12-01 05:51:08 +0100

    Use testset

--- test/methods/test_leanings.jl
+++ test/methods/test_leanings.jl
@@ -2,8 +2,10 @@
 x = [0, 0, 1, 0, 0, 1, 0, 0, 1, 0]
 y = [0, 0, 0, 1, 0, 0, 1, 0, 0, 1]
 
-@test mean_observed_penchant(x, y, 1, weighted = false) ≈ 1.0
-@test mean_observed_penchant(y, x, 1, weighted = false) ≈ 1/7
+@testset "Leanings" begin
+    @test mean_observed_penchant(x, y, 1, weighted = false) ≈ 1.0
+    @test mean_observed_penchant(y, x, 1, weighted = false) ≈ 1/7
 
-@test mean_observed_penchant(x, y, 1, weighted = true) ≈ 1.0
-@test mean_observed_penchant(y, x, 1, weighted = true) ≈ 3/63
+    @test mean_observed_penchant(x, y, 1, weighted = true) ≈ 1.0
+    @test mean_observed_penchant(y, x, 1, weighted = true) ≈ 3/63
+end
