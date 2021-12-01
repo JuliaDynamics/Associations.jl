de9ae08a8f07a36c5364714381dd0c28be0fbc4a
Kristian Agasøster Haaga <kahaaga@gmail.com>
2021-12-01 11:30:05 +0100

    Test Dataset explicitly

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,8 @@ using CausalityTools
 import DelayEmbeddings: Dataset
 import DynamicalSystemsBase: DiscreteDynamicalSystem, ContinuousDynamicalSystem
 
+x, y = rand(100), rand(100)
+@test Dataset(x, y) isa Dataset
 
 include("methods/test_smeasure.jl")
 include("methods/test_joint_distance_distribution.jl")
