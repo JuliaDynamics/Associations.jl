4feea5728a315f80e8d61388f63dcc5339578ce6
Kristian Haaga <kahaaga@gmail.com>
2022-08-21 09:18:07 +0200

    Better error message

--- src/ComplexityMeasures/compression_complexity/effort_to_compress/etc_multivariate.jl
+++ src/ComplexityMeasures/compression_complexity/effort_to_compress/etc_multivariate.jl
@@ -3,15 +3,15 @@ export symbol_sequence
 
 
 function compression_complexity(x::AbstractDataset{D, T}, algorithm::EffortToCompress) where {D, T}
-    !isnothing(algorithm.alphabet_size) || throw(ArgumentError("Alphabet size must be specified when input is multivariate"))
-    algorithm.alphabet_size >= 2 || throw(ArgumentError("Alphabet size must be at least 2."))
+    !isnothing(algorithm.alphabet_size) || throw(ArgumentError("Alphabet size must be specified when input is multivariate (e.g. do `compression_complexity(x, EffortToCompress(alphabet_size = 2)`"))
+    algorithm.alphabet_size >= 2 || throw(ArgumentError("Alphabet size must be at least 2."))
     encoded_sequence = symbol_sequence(x, algorithm.alphabet_size)
     return compression_complexity(encoded_sequence, algorithm)
 end
 
 function compression_complexity(x::AbstractDataset{D, T}, algorithm::EffortToCompressSlidingWindow) where {D, T}
-    !isnothing(algorithm.alphabet_size) || throw(ArgumentError("Alphabet size must be specified when input is multivariate"))
-    algorithm.alphabet_size >= 2 || throw(ArgumentError("Alphabet size must be at least 2."))
+    !isnothing(algorithm.alphabet_size) || throw(ArgumentError("Alphabet size must be specified when input is multivariate (e.g. do `compression_complexity(x, EffortToCompress(alphabet_size = 2)`"))
+    algorithm.alphabet_size >= 2 || throw(ArgumentError("Alphabet size must be at least 2."))
     
     encoded_sequence = symbol_sequence(x, algorithm.alphabet_size)
     windows = get_windows(x, algorithm.window_size, algorithm.step)
