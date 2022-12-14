0d1f3bd6ae968173bda477c736cf944ee4a360f4
Kristian Haaga <kahaaga@gmail.com>
2022-12-14 02:05:30 +0100

    Core API

--- src/core.jl
+++ src/core.jl
@@ -2,7 +2,7 @@ using DelayEmbeddings: AbstractDataset
 using Entropies: ProbabilitiesEstimator
 const Vector_or_Dataset{D, T} = Union{AbstractVector{T}, AbstractDataset{D, T}} where {D, T}
 
-export CausalityMeasure, InformationMeasure
+export CausalityMeasure
 
 """
     CausalityMeasure
@@ -11,20 +11,13 @@ The supertype of all causality measures.
 """
 abstract type CausalityMeasure end
 
-"""
-    InformationMeasure <: CausalityMeasure
 
-The supertype for all information-based measures.
+# # TODO: all methods shuld use something generic like this.
+# function estimate_from_marginals(measure::InformationMeasure, args...;
+#         kwargs...) where I <: InformationMeasure
+#     msg = "Marginal estimation of $I is not implemented"
+#     throw(ArgumentError(msg))
+# end
 
-"""
-abstract type InformationMeasure <: CausalityMeasure end
-
-# TODO: all methods shuld use something generic like this.
-function estimate_from_marginals(measure::InformationMeasure, args...;
-        kwargs...) where I <: InformationMeasure
-    msg = "Marginal estimation of $I is not implemented"
-    throw(ArgumentError(msg))
-end
-
-export estimate
-function estimate end
+# export estimate
+# function estimate end
