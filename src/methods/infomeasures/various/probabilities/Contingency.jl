aecbc11f7f692c93fb50a6ef06a5eed5d238e382
Kristian Haaga <kahaaga@gmail.com>
2023-02-02 02:46:26 +0100

    Update docstring

--- src/methods/infomeasures/various/probabilities/Contingency.jl
+++ src/methods/infomeasures/various/probabilities/Contingency.jl
@@ -10,6 +10,12 @@ probability mass function (internally represented as [`ContingencyMatrix`](@ref)
 It works directly on raw discrete/categorical data. Alternatively, if a
 [`ProbabilitiesEstimator`](@ref) `est` for which [`marginal_encodings`](@ref) is implemented
 is given, then input data are first discretized before creating the contingency matrix.
+
+!!! note
+    `Contingency` estimator differs from other [`ProbabilitiesEstimator`](@ref)s in that
+    it's not compatible with [`probabilities`](@ref) and other methods. Instead, it is
+    used to construct [`ContingencyMatrix`](@ref), from which probabilities can be
+    computed.
 """
 Base.@kwdef struct Contingency{E <: Union{Nothing, ProbabilitiesEstimator}} <: ProbabilitiesEstimator
     est::E = nothing
