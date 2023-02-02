d69119bd5a05347dd1810f7e2f5d5a2ac0e4fed8
Kristian Haaga <kahaaga@gmail.com>
2023-02-02 02:05:06 +0100

    Some "analytical in the limit" tests

--- test/independence/SurrogateTest.jl
+++ test/independence/SurrogateTest.jl
@@ -17,3 +17,49 @@ nsurr = 3
 @test independence(SurrogateTest(CMIShannon(), est; nsurr), food, likeit, service) isa SurrogateTestResult
 @test independence(SurrogateTest(CMIRenyiSarbu(), est; nsurr), food, likeit, service) isa SurrogateTestResult
 @test independence(SurrogateTest(CMIRenyiJizba(), est; nsurr), food, likeit, service) isa SurrogateTestResult
+
+
+# Analytical tests, in the limit.
+n = 100000
+
+# Pre-discretized data
+likeit = rand(["yes", "no"], n)
+food = rand(["veggies", "meat", "fish"], n)
+service = rand(["netflix", "hbo"], n)
+
+test_cmi = independence(SurrogateTest(CMIShannon(), est; nsurr), food, likeit, service)
+@test pvalue(test_cmi) > 0.05
+
+
+# Simulate a survey where the place a person grew up controls how many times they
+# fell while going skiing. The control happens through an intermediate variable
+# `preferred_equipment`, which indicates what type of physical activity the
+# person has engaged with. For this example, we should be able to reject
+# places ⫫ experience, but not reject places ⫫ experience | preferred_equipment
+α = 0.02 # pick some arbitrary significance level
+
+places = rand(["city", "countryside", "under a rock"], n);
+preferred_equipment = map(places) do place
+    if cmp(place, "city") == 1
+        return rand(["skateboard", "bmx bike"])
+    elseif cmp(place, "countryside") == 1
+        return rand(["sled", "snowcarpet"])
+    else
+        return rand(["private jet", "car"])
+    end
+end;
+experience = map(preferred_equipment) do equipment
+    if equipment ∈ ["skateboard", "bmx bike"]
+        return "didn't fall"
+    elseif equipment ∈ ["sled", "snowcarpet"]
+        return "fell 3 times or less"
+    else
+        return "fell uncontably many times"
+    end
+end;
+
+test_mi = independence(SurrogateTest(MIShannon(), est; nsurr), places, experience)
+@test pvalue(test_mi) < α
+
+test_cmi = independence(SurrogateTest(CMIShannon(), est; nsurr), places, experience, preferred_equipment)
+@test pvalue(test_cmi) > α
