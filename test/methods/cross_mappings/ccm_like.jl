a365d85d9f6de27086c7f1236a0ebcb8d8c1ea7c
Kristian Agasøster Haaga <kahaaga@gmail.com>
2023-02-07 16:14:41 +0100

    Explicitly provide negative lags to avoid warnings

--- test/methods/cross_mappings/ccm_like.jl
+++ test/methods/cross_mappings/ccm_like.jl
@@ -3,38 +3,39 @@ using CausalityTools
 using StateSpaceSets: Dataset
 x, y, z, w = rand(1000), rand(1000), Dataset(rand(1000, 3)), Dataset(rand(1001, 3))
 
+τ = -1
 @testset "Measures" begin
-    @test CCM() isa ConvergentCrossMapping
-    @test PairwiseAsymmetricInference() isa PairwiseAsymmetricInference
+    @test CCM(; τ) isa ConvergentCrossMapping
+    @test PairwiseAsymmetricInference(; τ) isa PairwiseAsymmetricInference
 end
 
 @testset "Basic" begin
-    @test crossmap(CCM(), x, y) isa Real
-    @test crossmap(PAI(), x, y) isa Real
+    @test crossmap(CCM(; τ), x, y) isa Real
+    @test crossmap(PAI(; τ), x, y) isa Real
     @test_throws AssertionError crossmap(PAI(), x, w)
 end
 
-@test crossmap(CCM(), ExpandingSegment(libsizes = 100), x, y) isa Real
-@test crossmap(CCM(), RandomSegment(libsizes = 100), x, y) isa Real
-@test crossmap(CCM(), RandomVectors(libsizes = 100, replace = false), x, y) isa Real
-@test crossmap(CCM(), RandomVectors(libsizes = 100, replace = true), x, y) isa Real
-@test crossmap(CCM(), ExpandingSegment(libsizes = 100:100:500), x, y) isa Vector{<:Real}
-@test crossmap(CCM(), RandomSegment(libsizes = 100:100:500), x, y) isa Vector{<:Real}
-@test crossmap(CCM(), RandomVectors(libsizes = 100:100:500, replace = false), x, y) isa Vector{<:Real}
-@test crossmap(CCM(), RandomVectors(libsizes = 100:100:500, replace = true), x, y) isa Vector{<:Real}
-@test_throws ArgumentError crossmap(CCM(), RandomSegment(libsizes = 100), x, w) isa Real
-@test_throws ArgumentError crossmap(CCM(), RandomVectors(libsizes = 100), x, w) isa Real
+@test crossmap(CCM(; τ), ExpandingSegment(libsizes = 100), x, y) isa Real
+@test crossmap(CCM(; τ), RandomSegment(libsizes = 100), x, y) isa Real
+@test crossmap(CCM(; τ), RandomVectors(libsizes = 100, replace = false), x, y) isa Real
+@test crossmap(CCM(; τ), RandomVectors(libsizes = 100, replace = true), x, y) isa Real
+@test crossmap(CCM(; τ), ExpandingSegment(libsizes = 100:100:500), x, y) isa Vector{<:Real}
+@test crossmap(CCM(; τ), RandomSegment(libsizes = 100:100:500), x, y) isa Vector{<:Real}
+@test crossmap(CCM(; τ), RandomVectors(libsizes = 100:100:500, replace = false), x, y) isa Vector{<:Real}
+@test crossmap(CCM(; τ), RandomVectors(libsizes = 100:100:500, replace = true), x, y) isa Vector{<:Real}
+@test_throws ArgumentError crossmap(CCM(; τ), RandomSegment(libsizes = 100), x, w) isa Real
+@test_throws ArgumentError crossmap(CCM(; τ), RandomVectors(libsizes = 100), x, w) isa Real
 
-@test crossmap(PAI(), ExpandingSegment(libsizes = 100), x, y) isa Real
-@test crossmap(PAI(), RandomSegment(libsizes = 100), x, y) isa Real
-@test crossmap(PAI(), RandomVectors(libsizes = 100, replace = false), x, y) isa Real
-@test crossmap(PAI(), RandomVectors(libsizes = 100, replace = true), x, y) isa Real
-@test crossmap(PAI(), ExpandingSegment(libsizes = 100:100:500), x, y) isa Vector{<:Real}
-@test crossmap(PAI(), RandomSegment(libsizes = 100:100:500), x, y) isa Vector{<:Real}
-@test crossmap(PAI(), RandomVectors(libsizes = 100:100:500, replace = false), x, y) isa Vector{<:Real}
-@test crossmap(PAI(), RandomVectors(libsizes = 100:100:500, replace = true), x, y) isa Vector{<:Real}
-@test_throws ArgumentError crossmap(PAI(), RandomSegment(libsizes = 100), x, w) isa Real
-@test_throws ArgumentError crossmap(PAI(), RandomVectors(libsizes = 100), x, w) isa Real
+@test crossmap(PAI(; τ), ExpandingSegment(libsizes = 100), x, y) isa Real
+@test crossmap(PAI(; τ), RandomSegment(libsizes = 100), x, y) isa Real
+@test crossmap(PAI(; τ), RandomVectors(libsizes = 100, replace = false), x, y) isa Real
+@test crossmap(PAI(; τ), RandomVectors(libsizes = 100, replace = true), x, y) isa Real
+@test crossmap(PAI(; τ), ExpandingSegment(libsizes = 100:100:500), x, y) isa Vector{<:Real}
+@test crossmap(PAI(; τ), RandomSegment(libsizes = 100:100:500), x, y) isa Vector{<:Real}
+@test crossmap(PAI(; τ), RandomVectors(libsizes = 100:100:500, replace = false), x, y) isa Vector{<:Real}
+@test crossmap(PAI(; τ), RandomVectors(libsizes = 100:100:500, replace = true), x, y) isa Vector{<:Real}
+@test_throws ArgumentError crossmap(PAI(; τ), RandomSegment(libsizes = 100), x, w) isa Real
+@test_throws ArgumentError crossmap(PAI(; τ), RandomVectors(libsizes = 100), x, w) isa Real
 
 # TODO: remove for v2.0
 @testset "Compat" begin
