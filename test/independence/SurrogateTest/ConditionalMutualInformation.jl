545e44eb376b12f55c0aec4b026785c75e003e44
Kristian Agasøster Haaga <kahaaga@gmail.com>
2023-02-07 16:18:41 +0100

    Fix tests

--- test/independence/SurrogateTest/ConditionalMutualInformation.jl
+++ test/independence/SurrogateTest/ConditionalMutualInformation.jl
@@ -1,5 +1,5 @@
 
-n = 100
+n = 200
 
 # Pre-discretized data
 likeit = rand(["yes", "no"], n)
@@ -15,25 +15,25 @@ nshuffles = 3
 
 # Analytical tests, in the limit.
 # -------------------------------
-n = 100000
+n = 10000
 
 # Pre-discretized data
 likeit = rand(["yes", "no"], n)
 food = rand(["veggies", "meat", "fish"], n)
 service = rand(["netflix", "hbo"], n)
 
+α = 0.05 # pick some arbitrary significance level
+
 # We should not be able to reject the null hypothesis `food ⫫ likeit | service`, because
 # the variables are all independent.
-test_cmi = independence(SurrogateTest(CMIShannon(), est; nshuffles), food, likeit, service)
-@test pvalue(test_cmi) > 0.05
-
+test_cmi = independence(SurrogateTest(CMIShannon(), est; nshuffles = 200), food, likeit, service)
+@test pvalue(test_cmi) > α
 
 # Simulate a survey where the place a person grew up controls how many times they
 # fell while going skiing. The control happens through an intermediate variable
 # `preferred_equipment`, which indicates what type of physical activity the
 # person has engaged with. For this example, we should be able to reject
 # places ⫫ experience, but not reject places ⫫ experience | preferred_equipment
-α = 0.02 # pick some arbitrary significance level
 
 places = rand(["city", "countryside", "under a rock"], n);
 preferred_equipment = map(places) do place
@@ -58,5 +58,6 @@ end;
 # We should not be able to reject the null hypothesis `places ⫫ experience | preferred_equipment`, because
 # places → preferred_equipment → experience, so when conditioning on the intermediate variable,
 # the first and last variable in the chain should be independent.
-test_cmi = independence(SurrogateTest(CMIShannon(), est; nshuffles), places, experience, preferred_equipment)
+test = SurrogateTest(CMIShannon(), est)
+test_cmi = independence(test, places, experience, preferred_equipment)
 @test pvalue(test_cmi) > α
