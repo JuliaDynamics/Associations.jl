8b4456728ac8b30ec499a8b47a7b4cb04c1af448
Kristian Agasøster Haaga <kahaaga@gmail.com>
2023-02-07 12:42:16 +0100

    Fix tests

--- test/independence/JointDistanceDistributionTest.jl
+++ test/independence/JointDistanceDistributionTest.jl
@@ -1,17 +1,17 @@
 using Random: MersenneTwister
 rng = MersenneTwister(1234)
-x, y = rand(rng, 1000), rand(rng, 1000)
-test =  JointDistanceDistributionTest()
+x, y = rand(rng, 3000), rand(rng, 3000)
+m = JointDistanceDistribution(D = 5, B = 10)
+test =  JointDistanceDistributionTest(m)
 @test test isa JointDistanceDistributionTest
 r = independence(test, x, y)
 @test r isa CausalityTools.JDDTestResult
 
 # Don't reject null at significance level (1 - α) when there is no coupling.
-α = 0.0001
+α = 0.01
 @test pvalue(r) > α 
 
 # Reject null at significance level (1 - α) when there is coupling
-x = randn(1000)
-y = 0.2randn(1000) .+ x
-r = independence(test, x, y)
-@test pvalue(r) < 0.0001
+z = y .+ x
+r = independence(test, z, x)
+@test pvalue(r) < α
