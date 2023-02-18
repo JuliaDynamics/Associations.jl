210fc359db61f37c732bd185c737f141b7236d53
Kristian Haaga <kahaaga@gmail.com>
2023-02-18 11:39:42 +0100

    Use generic signature

--- src/methods/infomeasures/transferentropy/transferentropy.jl
+++ src/methods/infomeasures/transferentropy/transferentropy.jl
@@ -98,7 +98,7 @@ function estimate(est::TE_ESTIMATORS, args...; kwargs...)
     estimate(TEShannon(), est, args...; kwargs...)
 end
 
-function transferentropy(measure::TransferEntropy, est, x...)
+function estimate(measure::TransferEntropy, est::TE_ESTIMATORS, x...)
     # If a conditional input (x[3]) is not provided, then C is just a 0-dimensional
     # dataset. The horizontal concatenation of C with T then just returns T.
     # We therefore don't need separate methods for the conditional and non-conditional
