4526fbdda8399477d5cb127526cce7d60a9a6bda
Kristian Haaga <kahaaga@gmail.com>
2023-02-18 11:33:12 +0100

    More tests

--- test/independence/SurrogateTest/TransferEntropyConditional.jl
+++ test/independence/SurrogateTest/TransferEntropyConditional.jl
@@ -1,9 +1,9 @@
-# A chain of coupled logistic maps. Set the coupling from first to second 
+# A chain of coupled logistic maps. Set the coupling from first to second
 # variable high, so that transferentropy(x → z) becomes significant.
 # This should vanish when doing transferentropy(x → z | y)
-sys = logistic4(c₁₂ = 0.6, u₀ = [0.1, 0.2, 0.3, 0.4]);
-n = 2000
-x, y,z, w = columns(trajectory(sys, n, Ttr = 10000));
+sys = system(Logistic4Chain(xi = [0.1, 0.2, 0.3, 0.4]));
+n = 500
+x, y, z, w = columns(trajectory(sys, n, Ttr = 10000));
 
 α = 0.04 # Arbitrary significance level 1 - α = 0.96
 
@@ -15,3 +15,6 @@ test = SurrogateTest(TEShannon(), FPVP())
 
 # We should be able to reject the null when testing transferentropy(x → y | z)
 @test pvalue(independence(test, x, z, y)) > α
+
+@test independence(SurrogateTest(TEShannon(), Zhu1()), x, y, z) isa SurrogateTestResult
+@test independence(SurrogateTest(TEShannon(), Lindner()), x, y, z) isa SurrogateTestResult
