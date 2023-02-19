2ba4314738cc47a49f91edd7ccce27d5a9006982
Kristian Haaga <kahaaga@gmail.com>
2023-02-19 21:09:40 +0100

    Tests

--- test/causal_graphs/pcrobust.jl
+++ test/causal_graphs/pcrobust.jl
@@ -1,3 +1,11 @@
-using Graphs
-x, y, z = randn(100), randn(100), randn(100)
-g = infer_graph(PCRobust())
+using Test
+using Graphs: SimpleDiGraph
+using Random; rng = Random.default_rng()
+
+x = randn(rng, 100), randn(rng, 100), randn(rng, 100)
+utest = SurrogateTest(MIShannon(), KSG1(); nshuffles = 4, rng)
+ctest = SurrogateTest(PartialCorrelation(); nshuffles = 4, rng)
+alg = PCRobust(utest, ctest)
+
+g = infer_graph(alg, x)
+@test g isa SimpleDiGraph
