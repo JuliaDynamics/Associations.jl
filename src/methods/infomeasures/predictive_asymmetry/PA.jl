28bea6021470cadc5ea743303c7828a6486b461a
Kristian Haaga <kahaaga@gmail.com>
2023-02-26 14:14:34 +0100

    Fix PATest.

--- src/methods/infomeasures/predictive_asymmetry/PA.jl
+++ src/methods/infomeasures/predictive_asymmetry/PA.jl
@@ -59,73 +59,23 @@ of the other dedicated conditional mutual information estimators.
 ## Examples
 
 - [Computing the asymmetry distribution](@ref examples_pa_asymmetry_dist).
-
 """
 struct PA{N, TS, TC} <: DirectedAssociationMeasure
-    ηT::N # multiple positive, unique integers
-    τS::TS # integer or range of positive integers
-    τC::TC# integer or range of positive integers
+    ηT::N # multiple positive, unique integers (or some instance of a delay optimiser)
+    τS::TS # integer or range of positive integers (or some instance of a delay optimiser)
+    τC::TC# integer or range of positive integers (or some instance of a delay optimiser)
 
     function PA(; ηT::N = 1:5, τS::TS = 1, τC::TC = 1) where {N, TS, TC}
-        all(ηT .> 0) || throw(ArgumentError("ηT must consist of integers >= 1"))
-        all(τS .> 0) || throw(ArgumentError("τS must consist of integers >= 1"))
-        all(τC .> 0) || throw(ArgumentError("τC must consist of integers >= 1"))
-        new{N, TS, TC}(ηT, τS, τC)
+       new{N, TS, TC}(ηT, τS, τC)
     end
 end
 
-# function embed(measure::PA, x, y)
-#     (; ηT, τS, τC) = measure
-#     ηT = sort(unique(ηT))
-#     τS = sort(unique(τS))
-#     nη = length(ηT)
-#     nτx = length(τS)
-#     jη = repeat([2], nη)
-#     js_x = repeat([1], nτx)
-#     js = [jη...; 2; jη...;     js_x...; 1; js_x...]
-#     τs = [ηT...; 0; .-(ηT)...; τS...; 0; .-(τS)...]
-#     joint = genembed(Dataset(x, y), τs, js)
-#     T⁺ = joint[:, 1:nη]
-#     T⁰ = joint[:, nη+1:nη+1]
-#     T⁻ = joint[:, nη+2:2nη+1]
-#     iₛ = 2nη+2
-#     S⁺ = joint[:, iₛ:(iₛ+nτx-1)]
-#     S⁰ = joint[:, (iₛ+nτx):(iₛ+nτx)]
-#     S⁻ = joint[:, (iₛ+nτx+1):end]
+# Optimize embedding lags. The following method just returns the given lags,
+# whatever form they may be in. Other methods of `optimize_embedding`
+# run some optimization procedure.
+optimize_embedding(τs, x) = τs
 
-#     return T⁺, T⁰, T⁻, S⁺, S⁰, S⁻
-# end
-
-# function embed(measure::PA, x, y, z)
-#     (; ηT, τS, τC) = measure
-#     ηT = sort(unique(ηT))
-#     τS = sort(unique(τS))
-#     τC = sort(unique(τC))
-#     nη = length(ηT)
-#     nτx = length(τS)
-#     nτz = length(τC)
-
-#     jη = repeat([2], nη)
-#     js_x = repeat([1], nτx)
-#     js_z = repeat([3], nτz)
-
-#     js = [jη...; 2; jη...;     js_x...; 1; js_x...;   js_z...; 1; js_z...]
-#     τs = [ηT...; 0; .-(ηT)...; τS...;   0; .-(τS)...; τC...;   0; .-(τC)...]
-#     joint = genembed(Dataset(x, y, z), τs, js)
-#     T⁺ = joint[:, 1:nη]
-#     T⁰ = joint[:, nη+1:nη+1]
-#     T⁻ = joint[:, nη+2:2nη+1]
-#     iₛ = 2nη+2
-#     S⁺ = joint[:, iₛ:(iₛ+nτx-1)]
-#     S⁰ = joint[:, (iₛ+nτx):(iₛ+nτx)]
-#     S⁻ = joint[:, (iₛ+nτx+1):(iₛ+2nτx)]
-#     ic = iₛ+2nτx+1
-#     C⁺ = joint[:, ic:(ic+nτz-1)]
-#     C⁰ = joint[:, (ic+nτz):(ic+nτz)]
-#     C⁻ = joint[:, (ic+nτz+1):(ic+2nτz)]
-
-#     return T⁺, T⁰, T⁻, S⁺, S⁰, S⁻, C⁺, C⁰, C⁻
-# end
+include("embeddings.jl")
 
 """
     asymmetry(measure::PA, est, x, y, [z]) → ΔA
@@ -152,70 +102,43 @@ function asymmetry(est::PA_ESTS, args...)
     throw(ArgumentError("A valid measure must be provided as the second argument; do `PA()`."))
 end
 
-
 function estimate(measure::PA, x::AbstractVector...)
     throw(ArgumentError("A valid estimator must be provided as the second argument, try for example `FPVP()`."))
 end
 
-# function estimate(measure::PA, est::PA_ESTS, x::AbstractVector, y::AbstractVector)
-#     (; ηT, τS, τC) = measure
+function redefine_lags(measure::PA, s)
+    if measure.τS isa Pecuzal
+        τS = optimize_embedding(measure.τS, s)
+    else
+        τS = measure.τS
+    end
+    return PA(; ηT = measure.ηT, τS = τS, τC = measure.τC)
+end
 
-#     T⁺, T⁰, T⁻, S⁺, S⁰, S⁻ = embed(measure, x, y)
-#     N = length(T⁺)
-#     S⁺⁰ = Dataset(S⁺, S⁰)
-#     S⁻⁰ = Dataset(S⁻, S⁰)
-#     ΔA = zeros(length(ηT))
-#     fw = zeros(length(ηT))
-#     bw = zeros(length(ηT))
-#     T⁺⁰ = MVector{2}.(Dataset(T⁰, T⁰).data)
-#     T⁻⁰ = MVector{2}.(Dataset(T⁰, T⁰).data)
+function redefine_lags(measure::PA, s, c)
+    if measure.τS isa Pecuzal
+        τS = optimize_embedding(measure.τS, s)
+    else
+        τS = measure.τS
+    end
 
-#     for (i, η) in enumerate(sort(ηT))
-#         fill_target!(T⁺⁰, T⁺, i)
-#         fill_target!(T⁻⁰, T⁻, i)
-#         fw[i] = condmutualinfo(CMIShannon(), est, Dataset(T⁺⁰), S⁻⁰, T⁻)
-#         bw[i] = condmutualinfo(CMIShannon(), est, Dataset(T⁻⁰), S⁺⁰, T⁺)
-#         ΔA[i] = fw[i] - bw[i]
-#     end
+    if measure.τC isa Pecuzal
+        τC = optimize_embedding(measure.τC, c)
+    else
+        τC = measure.τC
+    end
+    return PA(; ηT = measure.ηT, τS = τS, τC = τC)
+end
 
-#     return ΔA
-# end
-
-# function estimate(measure::PA, est::PA_ESTIMATORS,
-#         x::AbstractVector, y::AbstractVector, z::AbstractVector)
-#     (; ηT, τS, τC) = measure
-#     T⁺, T⁰, T⁻, S⁺, S⁰, S⁻, C⁺, C⁰, C⁻ = embed(measure, x, y, z)
-#     N = length(T⁺)
-#     S⁺⁰ = Dataset(S⁺, S⁰)
-#     S⁻⁰ = Dataset(S⁻, S⁰)
-#     C⁺⁰ = Dataset(C⁺, C⁰)
-#     C⁻⁰ = Dataset(C⁻, C⁰)
-
-#     ΔA = zeros(length(ηT))
-#     fw = zeros(length(ηT))
-#     bw = zeros(length(ηT))
-#     T⁺⁰ = MVector{2}.(Dataset(T⁰, T⁰).data)
-#     T⁻⁰ = MVector{2}.(Dataset(T⁰, T⁰).data)
-#     TC⁻⁰ = Dataset(T⁻, C⁻⁰)
-#     TC⁺⁰ = Dataset(T⁺, C⁺⁰)
-
-#     for (i, η) in enumerate(sort(ηT))
-#         fill_target!(T⁺⁰, T⁺, i)
-#         fill_target!(T⁻⁰, T⁻, i)
-#         fw[i] = condmutualinfo(CMIShannon(), est, Dataset(T⁺⁰), S⁻⁰, TC⁻⁰)
-#         bw[i] = condmutualinfo(CMIShannon(), est, Dataset(T⁻⁰), S⁺⁰, TC⁺⁰)
-#         ΔA[i] = fw[i] - bw[i]
-#     end
-
-#     return ΔA
-# end
-
-# With instantaneous coupling
-#----------------------------------------------------------------
 function embed(measure::PA, x, y)
     (; ηT, τS, τC) = measure
+
     ηT = sort(unique(ηT))
     τS = sort(unique([τS; ηT]))
+    all(ηT .> 0) || throw(ArgumentError("Lags must be positive integers. Got ηT=$ηT."))
+    all(τS .> 0) || throw(ArgumentError("Lags must be positive integers. Got τS=$τS."))
+    all(τC .> 0) || throw(ArgumentError("Lags must be positive integers. Got τC=$τC."))
+
     nη = length(ηT)
     nτx = length(τS)
     jη = repeat([2], nη)
@@ -238,7 +161,10 @@ function embed(measure::PA, x, y, z)
     (; ηT, τS, τC) = measure
     ηT = sort(unique(ηT))
     τS = sort(unique([τS; ηT]))
-    τC = sort(unique(τC))
+    τC = sort(unique([τC; ηT]))
+    all(ηT .> 0) || throw(ArgumentError("Lags must be positive integers. Got ηT=$ηT."))
+    all(τS .> 0) || throw(ArgumentError("Lags must be positive integers. Got τS=$τS."))
+    all(τC .> 0) || throw(ArgumentError("Lags must be positive integers. Got τC=$τC."))
     nη = length(ηT)
     nτx = length(τS)
     nτz = length(τC)
@@ -267,14 +193,15 @@ end
 
 as_vector(x::Union{AbstractVector{T}}) where T = vec(x)
 as_vector(x::AbstractDataset{1, T}) where T = x[:, 1]
+
 function estimate(measure::PA, est::PA_ESTS, x, y)
-    (; ηT, τS, τC) = measure
     X = as_vector(x)
     Y = as_vector(y)
-    measure = redefine(measure, x)
+    measure = redefine_lags(measure, x)
     (; ηT, τS, τC) = measure
 
     T⁺, T⁰, T⁻, S⁺, S⁰, S⁻ = embed(measure, X, Y)
+
     N = length(T⁺)
     τŜ = sort(unique([τS; ηT]))
     S⁺⁰ = Dataset(S⁺[:, findall(x -> x ∈ τS, τŜ)], S⁰)
@@ -282,13 +209,17 @@ function estimate(measure::PA, est::PA_ESTS, x, y)
     ΔA = zeros(length(ηT))
     fw = zeros(length(ηT))
     bw = zeros(length(ηT))
-    Tⁿ⁺⁰ = MVector{2}.(Dataset(T⁰, T⁰).data)
-    Tⁿ⁻⁰ = MVector{2}.(Dataset(T⁰, T⁰).data)
+    T⁺⁰ = MVector{2}.(Dataset(T⁰, T⁰).data)
+    T⁻⁰ = MVector{2}.(Dataset(T⁰, T⁰).data)
+    Tⁿ⁺ = MVector{1}.(Dataset(T⁰).data)
+    Tⁿ⁻ = MVector{1}.(Dataset(T⁰).data)
     for (i, η) in enumerate(sort(ηT))
-        fill_target!(Tⁿ⁺⁰, T⁺, i)
-        fill_target!(Tⁿ⁻⁰, T⁻, i)
-        fw[i] = condmutualinfo(CMIShannon(), est, Tⁿ⁺⁰, S⁻⁰, T⁻)
-        bw[i] = condmutualinfo(CMIShannon(), est, Tⁿ⁻⁰, S⁺⁰, T⁺)
+        fill_target!(Tⁿ⁺, T⁺, i)
+        fill_target!(Tⁿ⁻, T⁻, i)
+        fill_target!(T⁺⁰, T⁺, i)
+        fill_target!(T⁻⁰, T⁻, i)
+        fw[i] = condmutualinfo(CMIShannon(), est, Tⁿ⁺, S⁻⁰, T⁻⁰)
+        bw[i] = condmutualinfo(CMIShannon(), est, Tⁿ⁻, S⁺⁰, T⁺⁰)
         ΔA[i] = fw[i] - bw[i]
     end
 
@@ -296,7 +227,7 @@ function estimate(measure::PA, est::PA_ESTS, x, y)
 end
 
 function estimate(measure::PA, est::PA_ESTIMATORS, x, y, z)
-    measure = redefine(measure, x, z)
+    measure = redefine_lags(measure, x, z)
     (; ηT, τS, τC) = measure
     X = as_vector(x)
     Y = as_vector(y)
@@ -304,24 +235,27 @@ function estimate(measure::PA, est::PA_ESTIMATORS, x, y, z)
     T⁺, T⁰, T⁻, S⁺, S⁰, S⁻, C⁺, C⁰, C⁻ = embed(measure, X, Y, Z)
 
     N = length(T⁺)
-    S⁺⁰ = Dataset(S⁺, S⁰)
-    S⁻⁰ = Dataset(S⁻, S⁰)
-    C⁺⁰ = Dataset(C⁺, C⁰)
-    C⁻⁰ = Dataset(C⁻, C⁰)
-
+    τŜ = sort(unique([τS; ηT]))
+    S⁺⁰ = Dataset(S⁺[:, findall(x -> x ∈ τS, τŜ)], S⁰)
+    S⁻⁰ = Dataset(S⁻[:, findall(x -> x ∈ τS, τŜ)], S⁰)
+    τĈ = sort(unique([τC; ηT]))
+    C⁺⁰ = Dataset(C⁺[:, findall(x -> x ∈ τC, τĈ)], C⁰)
+    C⁻⁰ = Dataset(C⁻[:, findall(x -> x ∈ τC, τĈ)], C⁰)
     ΔA = zeros(length(ηT))
     fw = zeros(length(ηT))
     bw = zeros(length(ηT))
-    T⁺⁰ = MVector{2}.(Dataset(T⁰, T⁰).data)
-    T⁻⁰ = MVector{2}.(Dataset(T⁰, T⁰).data)
-    T⁻C⁻⁰ = Dataset(T⁻, C⁻⁰)
-    T⁺C⁺⁰ = Dataset(T⁺, C⁺⁰)
+    Tⁿ⁺⁰C⁺⁰ = MVector{2 + dimension(C⁻⁰)}.(Dataset(T⁰, T⁰, C⁻⁰).data)
+    Tⁿ⁻⁰C⁻⁰ = MVector{2 + dimension(C⁺⁰)}.(Dataset(T⁰, T⁰, C⁺⁰).data)
+    Tⁿ⁺ = MVector{1}.(T⁰.data)
+    Tⁿ⁻ = MVector{1}.(T⁰.data)
 
     for (i, η) in enumerate(sort(ηT))
-        fill_target!(T⁺⁰, T⁺, i)
-        fill_target!(T⁻⁰, T⁻, i)
-        fw[i] = condmutualinfo(CMIShannon(), est, T⁺⁰, S⁻⁰, T⁻C⁻⁰)
-        bw[i] = condmutualinfo(CMIShannon(), est, T⁻⁰, S⁺⁰, T⁺C⁺⁰)
+        fill_target!(Tⁿ⁺, T⁺, i)
+        fill_target!(Tⁿ⁻, T⁻, i)
+        fill_target!(Tⁿ⁺⁰C⁺⁰, T⁺, i)
+        fill_target!(Tⁿ⁻⁰C⁻⁰, T⁺, i)
+        fw[i] = condmutualinfo(CMIShannon(), est, Tⁿ⁺, S⁻⁰, Tⁿ⁻⁰C⁻⁰)
+        bw[i] = condmutualinfo(CMIShannon(), est, Tⁿ⁻, S⁺⁰, Tⁿ⁺⁰C⁺⁰)
         ΔA[i] = fw[i] - bw[i]
     end
 
