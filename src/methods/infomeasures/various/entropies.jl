d726404472640639f7cfb0f7dad501b19a561632
Kristian Haaga <kahaaga@gmail.com>
2023-03-01 15:54:53 +0100

    Remove reference to deleted file

--- src/methods/infomeasures/various/entropies.jl
+++ src/methods/infomeasures/various/entropies.jl
@@ -1,5 +1,4 @@
 include("entropies/LeonenkoProzantoSavani.jl")
-include("entropies/Lord.jl")
 include("entropies/GenericKernel.jl")
 include("entropies/Faivishevsky.jl")
 include("entropies/Pal.jl")
