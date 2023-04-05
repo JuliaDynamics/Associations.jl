297472cacaa643fcabe87e88dbc09f618ab073e3
Kristian Haaga <kahaaga@gmail.com>
2023-04-05 12:16:16 +0200

    Update tests

--- test/causal_graphs/oce.jl
+++ test/causal_graphs/oce.jl
@@ -15,21 +15,20 @@ parents = infer_graph(alg, X; verbose = true)
 @test SimpleDiGraph(parents) isa SimpleDiGraph
 
 rng = StableRNG(123)
-sys = system(CommonCauseMutual(; rng))
-X = columns(trajectory(sys, 200, Ttr = 10000))
+sys = system(Logistic2Bidir(; rng))
+X = columns(trajectory(sys, 300, Ttr = 10000))
 utest = SurrogateTest(MIShannon(), KSG1(k = 10, w = 1); rng, nshuffles = 100)
 ctest = LocalPermutationTest(CMIShannon(), MesnerShalisi(k = 10, w = 1); rng, nshuffles = 100)
 parents = infer_graph(OCE(; utest, ctest, τmax = 1), X; verbose = true)
 @test parents isa Vector{<:OCESelectedParents}
 g = SimpleDiGraph(parents)
 @test g isa SimpleDiGraph
+
 # "Analytical" test: check that we at least identify one true positive. There may
 # be several false positives, but there's no way of telling a priori how many.
-function at_least_one_true_positive(sys, estimated_graph)
+function at_least_one_true_positive(true_edges, estimated_graph)
     estimated_edges = edges(estimated_graph)
-    true_edges = edges(SimpleDiGraph(sys))
     at_least_one_tp = false
-
     for e in true_edges
         if e in estimated_edges
             at_least_one_tp = true
@@ -38,4 +37,4 @@ function at_least_one_true_positive(sys, estimated_graph)
     return at_least_one_tp
 end
 
-@test at_least_one_true_positive(CommonCauseMutual(), g)
+@test at_least_one_true_positive([SimpleEdge(1, 2), SimpleEdge(2, 1)], g)
