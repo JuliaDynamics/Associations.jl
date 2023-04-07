f7c02c18f47bd27520b8a92f685e4bd2b93eee93
Kristian Haaga <kahaaga@gmail.com>
2023-04-08 01:23:01 +0200

    More tests

--- test/methods/infomeasures/pmi.jl
+++ test/methods/infomeasures/pmi.jl
@@ -10,4 +10,9 @@ x, y, z, w = columns(trajectory(sys, 1000, Ttr = 10000))
 @test estimate(PMI(), esth, x, w, Dataset(z, y)) >= 0
 
 @test pmi(PMI(base = 3), esth, x, w, z) >= 0
-@test pmi(esth, x, w, z) >= 0
+@test pmi(SymbolicPermutation(m = 3), x, w, z) >= 0
+
+x = rand(rng, 1:3, 20000)
+y = rand(rng, 1:3, 20000)
+z = rand(rng, 1:3, 20000)
+@test round(pmi(CountOccurrences(), x, y, z), digits = 3) == 0
