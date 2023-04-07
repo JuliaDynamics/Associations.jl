70809b83fc06fcc0d98265a652e83f80adde6102
Kristian Haaga <kahaaga@gmail.com>
2023-04-08 00:56:48 +0200

    Docs and convenience methods

--- src/methods/infomeasures/pmi.jl
+++ src/methods/infomeasures/pmi.jl
@@ -4,11 +4,25 @@ export pmi
 """
     PMI <: AssociationMeasure
 
-The partial mutual information (PMI) measure of association.
+The partial mutual information (PMI) measure of association (Zhao et al., 2016)[^Zhao2016].
+
+## Definition
+
+PMI is defined as for variables ``X``, ``Y`` and ``Z`` as
+
+```math
+PMI(X; Y | Z) = D(p(x, y, z) || p^{*}(x|z) p^{*}(y|z) p(z)),
+```
+
+where ``p(x, y, z)`` is the joint distribution for ``X``, ``Y`` and ``Z``, and
+``D(\\cdot, \\cdot)`` is the extended Kullback-Leibler divergence from
+``p(x, y, z)`` to ``p^{*}(x|z) p^{*}(y|z) p(z)``. See Zhao et al., 2016 for details.
 
 ## Estimation
 
-PMI can be estimated using any estimator that implements [`marginal_encodings`](@ref).
+PMI can be estimated using any [`ProbabilitiesEstimator`](@ref) that implements
+[`marginal_encodings`](@ref). This allows estimation of 3D contingency matrices, from
+which relevant probabilities for the PMI formula are extracted. See also [`pmi`](@ref).
 
 ## Properties
 
@@ -21,13 +35,49 @@ may not).
     [`ProbabilitiesEstimator`](@ref)s.
 - `X ⫫ Y | Z => PMI(X, Y, Z) = CMI(X, Y, Z) = 0` (in theory, but not necessarily for
     estimation).
+
+[^Zhao2016]:
+    Zhao, J., Zhou, Y., Zhang, X., & Chen, L. (2016). Part mutual information for
+    quantifying direct associations in networks. Proceedings of the National Academy
+    of Sciences, 113(18), 5130-5135.
 """
 Base.@kwdef struct PMI <: AssociationMeasure
     base::Real = 2
 end
 
+"""
+    pmi([measure::CMI], est::ProbabilitiesEstimator, x, y, z) → pmi_est::Real ∈ [0, a)
+
+Estimate the part mutual information ([`PMI`](@ref); Zhao et al. (2016)[^Zhao2016]).
+
+If `measure` is not given, then the default is `PMI(; base = 2)`.
+With a [`ProbabilitiesEstimator`](@ref), the returned `pmi_est` is guaranteed to be
+non-negative.
+
+## Estimators
+
+| Estimator                     | Principle           | [`PMI`](@ref) |
+| ----------------------------- | ------------------- | :-----------: |
+| [`CountOccurrences`](@ref)    | Frequencies         |          ✓    |
+| [`ValueHistogram`](@ref)      | Binning (histogram) |          ✓    |
+| [`SymbolicPermutation`](@ref) | Ordinal patterns    |          ✓    |
+| [`Dispersion`](@ref)          | Dispersion patterns |          ✓    |
+
+[^Zhao2016]:
+    Zhao, J., Zhou, Y., Zhang, X., & Chen, L. (2016). Part mutual information for
+    quantifying direct associations in networks. Proceedings of the National Academy
+    of Sciences, 113(18), 5130-5135.
+"""
+function pmi(measure::PMI, x...)
+    return estimate(measure, x...)
+end
+
 function pmi(x...)
     return estimate(PMI(), x...)
+end
+
+function estimate(measure::PMI, est::ProbabilitiesEstimator, x...)
+    return estimate(measure, contingency_matrix(est, x...))
 end
 
 function estimate(measure::PMI, est::Contingency{<:ProbabilitiesEstimator}, x...)
