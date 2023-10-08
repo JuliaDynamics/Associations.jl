474db7ed5704bc815ea7c35b487d3a574fb3287f
Kristian Agasøster Haaga <kahaaga@gmail.com>
2023-10-08 19:23:11 +0200

    Generic divergence estimation

--- src/information/information_definitions/divergences_and_distances/divergences_and_distances.jl
+++ src/information/information_definitions/divergences_and_distances/divergences_and_distances.jl
@@ -1,5 +1,12 @@
 abstract type DivergenceOrDistance <: BivariateInformationMeasure end
 
+# If a joint probability is given, get the marginals
+function information(measure::DivergenceOrDistance, p::Probabilities{T, 2}) where T
+    px = marginal(p, dims = 1)
+    py = marginal(p, dims = 2)
+    return information(measure, px, py)
+end
+
 include("KLDivergence.jl")
 include("RenyiDivergence.jl")
 include("HellingerDistance.jl")
