116601049437bd6f86994c933fdf8d96c71bbcec
Kristian Agasøster Haaga <kahaaga@gmail.com>
2023-10-08 19:18:56 +0200

    Less restrictive type requirements

--- src/core/estimation_per_point.jl
+++ src/core/estimation_per_point.jl
@@ -4,7 +4,7 @@ import ComplexityMeasures: probabilities
 
 # If multiple encodings are given, the number of encodings must match the number of
 # input variables.
-function counts(encoding::CodifyPoints{N}, x::Vararg{T, N}) where {T, N}
+function counts(encoding::CodifyPoints{N}, x::Vararg{Any, N}) where {N}
     x̂ = codify(encoding, x...)
     return counts(UniqueElements(), x̂...)
 end
@@ -17,12 +17,12 @@ function counts(encoding::CodifyPoints{1}, x::Vararg{Any, N}) where {Any, N}
 end
 
 # If only one encoding is given, apply same encoding to all points
-function probabilities(encoding::CodifyPoints{1}, x::Vararg{T, N}) where {T, N}
+function probabilities(encoding::CodifyPoints{1}, x::Vararg{Any, N}) where {N}
     cts = counts(encoding, x...)
     return Probabilities(cts)
 end
 
-function probabilities(encoding::CodifyPoints{N}, x::Vararg{Any, N}) where {T, N}
+function probabilities(encoding::CodifyPoints{N}, x::Vararg{Any, N}) where {N}
     cts = counts(encoding, x...)
     return Probabilities(cts)
 end
