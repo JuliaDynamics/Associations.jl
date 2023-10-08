3fc5738c9a60fad453922ee3606eb3f642c6aa9f
Kristian Agasøster Haaga <kahaaga@gmail.com>
2023-10-08 19:19:40 +0200

    Load estimators before definitions

--- src/information/information.jl
+++ src/information/information.jl
@@ -3,5 +3,5 @@ import ComplexityMeasures: information
 # These files extend the single-variable information API in ComplexityMeasures.jl.
 include("information_measures.jl")
 include("information_functions.jl")
-include("information_definitions/information_definitions.jl")
 include("information_estimators/information_estimators.jl")
+include("information_definitions/information_definitions.jl")
