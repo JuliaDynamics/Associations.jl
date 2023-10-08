3af3204a32fee91ad12cae8393519c9f392ef1da
Kristian Agasøster Haaga <kahaaga@gmail.com>
2023-10-08 19:22:00 +0200

    JointProbabilities estimator

--- src/information/information_estimators/Contingency.jl
+++ src/information/information_estimators/Contingency.jl
@@ -1,23 +1,39 @@
-export Contingency
+using ComplexityMeasures: OutcomeSpace
+export JointProbabilities
 
 """
-    Contingency <: InformationMeasureEstimator
-    Contingency(definition::MultivariateInformationMeasure,
-        pest::ProbabilitiesEstimator, o::OutcomeSpace)
+    JointProbabilities <: InformationMeasureEstimator
+    JointProbabilities(
+        definition::MultivariateInformationMeasure,
+        discretization::Discretization
+    )
 
-`Contingency` is a generic estimator for discrete information measures that first
-discretizes/encodes the input data according to the given [`OutcomeSpace`](@ref) `o`, then
+`JointProbabilities` is a generic estimator for discrete information measures that first
+discretizes/encodes the input data according to the `discretization` (either a 
+[`CodifyVariables`](@ref) in the case of time series or the more generic
+[`CodifyPoints`](@ref) in other cases), then
 constructs a contingency table of the required dimensionality (a [`Counts`](@ref) instance),
 then constructs a multidimensional probability mass function (a [`Probabilities`](@ref)
-instance) from those counts using the provided probabilities estimator `pest`.
+instance) using plug-in estimation of probabilities (relative frequencies of counts).
 
-Works for any outcome space that implements [`symbolize`](@ref).
+Works for any outcome space that implements [`codify`](@ref).
 
-See also: [`Counts`](@ref), [`Probabilities]](@ref), [`ProbabilitiesEstimator`](@ref),
+See also: [`Counts`](@ref), [`Probabilities`](@ref), [`ProbabilitiesEstimator`](@ref),
 [`OutcomeSpace`](@ref), [`DiscreteInfoEstimator`](@ref).
 """
-Base.@kwdef struct Contingency{M <: MultivariateInformationMeasure, O, P} <: InformationMeasureEstimator{M}
-    definition::M # API from complexity measures: definition must be first field.
-    o::O
-    pest::P
+struct JointProbabilities{M <: MultivariateInformationMeasure, O, P} <: InformationMeasureEstimator{M}
+    definition::M # API from complexity measures: definition must be the first field of the infoestimator.
+    discretization::O
+    pest::P # Not exposed for user for now.
+
+    function JointProbabilities(def::M, disc::D, pest = RelativeAmount()) where {M, D}
+        new{M, D, typeof(pest)}(def, disc, pest)
+    end
+end
+
+# One method to compute them all (through joint probabilities)
+const DiscretizationOrOutcomeSpace = Union{Discretization, OutcomeSpace}
+function information(est::JointProbabilities{<:M, <:DiscretizationOrOutcomeSpace}, x...) where M
+    probs = probabilities(est.discretization, x...)
+    return information(est.definition, probs)
 end
