f0aa61134d8f2d355798b06a5d140f65d7115682
Kristian Agasøster Haaga <kahaaga@gmail.com>
2023-10-12 13:06:33 +0200

    Fix tests

--- test/information/mutual_informations/api.jl
+++ test/information/mutual_informations/api.jl
@@ -29,10 +29,10 @@ end
 # The following measures can be estimated using an entropy decomposition
 defs = [MIShannon(), MITsallisMartin(), MITsallisFuruichi()]
 ests = [PlugIn(Shannon()), PlugIn(Tsallis(q = 1.5)), PlugIn(Tsallis(q = 1.5))]
-@testset "DiscreteDecomposition with $(typeof(def).name.name)" for (def, est) in zip(defs, ests)
+@testset "EntropyDecomposition with $(typeof(def).name.name)" for (def, est) in zip(defs, ests)
     x, y = rand(rng, 100), rand(rng, 100)
     X, Y = StateSpaceSet(rand(rng, 100, 2)), StateSpaceSet(rand(rng, 100, 2))
-    est = DiscreteDecomposition(def, est, OrdinalPatterns(m=2), RelativeAmount())
+    est = EntropyDecomposition(def, est, OrdinalPatterns(m=2), RelativeAmount())
     #@show est
     # The estimation of probabilities is decoupled from the estimation of the mutual info.
     # We could in principle use any probabilities estimator here, but we default to `RelativeAmount`.
@@ -44,12 +44,12 @@ end
 
 # The following measures cannot be decomposed into entropies and should throw errors
 definitions = [MIRenyiSarbu()]
-@testset "DiscreteDecomposition with $(typeof(def).name.name)" for def in definitions
+@testset "EntropyDecomposition with $(typeof(def).name.name)" for def in definitions
     x, y = rand(rng, 100), rand(rng, 100)
     X, Y = StateSpaceSet(rand(rng, 100, 2)), StateSpaceSet(rand(rng, 100, 2))
     
-    est_diff = DifferentialDecomposition(def, Kraskov(k=3))
-    est_disc = DiscreteDecomposition(def, PlugIn(Shannon()), OrdinalPatterns(m=2), RelativeAmount())
+    est_diff = EntropyDecomposition(def, Kraskov(k=3))
+    est_disc = EntropyDecomposition(def, PlugIn(Shannon()), OrdinalPatterns(m=2), RelativeAmount())
 
     @test_throws ArgumentError information(est_diff, x, Y)
     @test_throws ArgumentError information(est_diff, X, y)
