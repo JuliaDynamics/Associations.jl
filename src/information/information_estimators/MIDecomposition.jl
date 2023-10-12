010d029f36bf3e91394857f7665174a174e63b57
Kristian Agasøster Haaga <kahaaga@gmail.com>
2023-10-12 14:57:18 +0200

    Complete docstring for `MIDecomposition``

--- src/information/information_estimators/MIDecomposition.jl
+++ src/information/information_estimators/MIDecomposition.jl
@@ -6,11 +6,28 @@ export MIDecomposition
         est::MutualInformationEstimator)
 
 Estimate some multivariate information measure specified by `definition`, by decomposing
-it into a combination of mutual information terms, which are estimated using `est`.
+it into a combination of mutual information terms, which are estimated using `est`,
+which can be any [`MutualInformationEstimator`](@ref).
+
+Similar to [`EntropyDecomposition`](@ref), but `definition` is expressed as 
+mutual information terms instead of entropy terms.
 
 ## Usage
 
 - [`information`](@ref)`(est::MIDecomposition, x...)`.
+
+## Example 
+
+```julia
+using CausalityTools
+using Random; rng = MersenneTwister(1234)
+x = rand(rng, 100000)
+y = rand(rng, 100000) .+ x
+z = rand(rng, 100000) .+ y
+
+est = MIDecomposition(CMIShannon(), KSG1(MIShannon(base = 2), k = 3))
+information(est, x, z, y) # should be near 0 (and can be negative)
+```
 
 See also: [`MutualInformationEstimator`](@ref), [`MultivariateInformationMeasure`](@ref).
 """
