d6f05cac0347e4ef4bb99ca626379693942c5118
Kristian Agasøster Haaga <kahaaga@gmail.com>
2023-10-14 02:16:38 +0200

    Remove old files

--- src/methods/infomeasures/mutualinfo/estimators/estimators.jl
+++
@@ -1,9 +0,0 @@
-include("utils.jl")
-
-include("KSG1.jl")
-include("KSG2.jl")
-include("GaoOhViswanath.jl")
-include("GaoKannanOhViswanath.jl")
-include("GaussianMI.jl")
-include("wip/copula/copula_nonparametric.jl")
-include("wip/copula/copula_parametric.jl")
