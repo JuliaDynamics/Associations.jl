f64e6cac4efec952ac06c0201d1ca441c05d7c6c
Kristian Agasøster Haaga <kahaaga@gmail.com>
2024-07-14 20:38:39 +0200

    Fix import

--- test/methods/correlation/partial_correlation.jl
+++ test/methods/correlation/partial_correlation.jl
@@ -1,9 +1,9 @@
 using Test
-using StatsBase
+using StatsBase: partialcor
 x = rand(100)
 y = rand(100)
 z = rand(100, 2)
 
-@test association(PartialCorrelation(), x, y, z) ≈ StatsBase.partialcor(x, y, z)
+@test association(PartialCorrelation(), x, y, z) ≈ partialcor(x, y, z)
 
 @test_logs (:warn, "Convenience function `partial_correlation` is deprecated. Use `association(PartialCorrelation(), x, y, z)` instead.") partial_correlation(x, y, z)
