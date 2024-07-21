034114b8166ce1d021f90af3876b28f902276a61
Kristian Agasøster Haaga <kahaaga@gmail.com>
2024-07-21 22:54:27 +0200

    Fewer samples -> faster tests

--- test/independence/SurrogateAssociationTest/crossmappings.jl
+++ test/independence/SurrogateAssociationTest/crossmappings.jl
@@ -1,16 +1,21 @@
 using Test
 using Random
 rng = MersenneTwister(1234)
-x, y = rand(rng, 500), rand(rng, 500)
+n = 200
+x, y = rand(rng, n), rand(rng, n)
 z = x.+ y
 
 d = 2
 τ = -1
 
 # Regular variant.
-test_ccm = SurrogateAssociationTest(RandomVectors(CCM(; d, τ); libsizes = 300, replace = true, rng))
-test_pai = SurrogateAssociationTest(RandomVectors(PAI(; d, τ); libsizes = 300, replace = true, rng))
-@test_throws ArgumentError SurrogateAssociationTest(Ensemble(CCM(), RandomVectors(libsizes = 100:100:300)))
+est_ccm = RandomVectors(CCM(; d, τ); libsizes = 100, replace = true, rng)
+test_ccm = SurrogateAssociationTest(est_ccm; nshuffles = 19)
+
+est_pai = RandomVectors(PAI(; d, τ); libsizes = 100, replace = true, rng)
+test_pai = SurrogateAssociationTest(est_pai; nshuffles = 19)
+
+# Invalid syntax.
 @test_throws ArgumentError SurrogateAssociationTest(CCM(), RandomVectors(libsizes = 100:100:300))
 
 α = 0.03 # arbitrarily set confidence level to 1 - α
@@ -20,10 +25,10 @@ test_pai = SurrogateAssociationTest(RandomVectors(PAI(; d, τ); libsizes = 300, 
 @test pvalue(independence(test_pai, x, y)) > α
 
 # Ensemble variant.
-eccm = Ensemble(RandomVectors(CCM(; d, τ); libsizes = 100, replace = true, rng))
-epai = Ensemble(RandomVectors(PAI(; d, τ); libsizes = 100, replace = true, rng))
-test_ccm = SurrogateAssociationTest(eccm)
-test_pai = SurrogateAssociationTest(epai)
+eccm = Ensemble(RandomVectors(CCM(; d, τ); libsizes = 50, replace = true, rng))
+epai = Ensemble(RandomVectors(PAI(; d, τ); libsizes = 50, replace = true, rng))
+test_ccm = SurrogateAssociationTest(eccm; nshuffles = 19)
+test_pai = SurrogateAssociationTest(epai; nshuffles = 19)
 @test pvalue(independence(test_ccm, x, y)) > α
 @test pvalue(independence(test_ccm, x, z)) < α
 @test pvalue(independence(test_ccm, x, z)) < α
