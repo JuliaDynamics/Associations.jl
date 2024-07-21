fd557953b27dfd2e3432ad080715ade94c8e12f4
Kristian Agasøster Haaga <kahaaga@gmail.com>
2024-07-21 22:03:34 +0200

    Fix tests

--- test/independence/SurrogateAssociationTest/TransferEntropyConditional.jl
+++ test/independence/SurrogateAssociationTest/TransferEntropyConditional.jl
@@ -4,19 +4,22 @@
 using Random
 rng = Random.MersenneTwister(1234)
 sys = system(Logistic4Chain(; xi = [0.1, 0.2, 0.3, 0.4], rng));
-n = 500
+n = 300
 x, y, z, w = columns(first(trajectory(sys, n, Ttr = 10000)));
 
 α = 0.04 # Arbitrary significance level 1 - α = 0.96
 
 # The ground truth is X → Y → Z.
-test = SurrogateAssociationTest(TEShannon(), FPVP(); rng)
-
+est = CMIDecomposition(TEShannon(), FPVP())
+test = SurrogateAssociationTest(est; rng, nshuffles = 19)
 
 @test pvalue(independence(test, x, z)) < α # This has been manually tested to occur with c₁₂ = 0.8
 
 # We should be able to reject the null when testing transferentropy(x → y | z)
 @test pvalue(independence(test, x, z, y)) > α
 
-@test independence(SurrogateAssociationTest(TEShannon(), Zhu1()), x, y, z) isa SurrogateAssociationTestResult
-@test independence(SurrogateAssociationTest(TEShannon(), Lindner()), x, y, z) isa SurrogateAssociationTestResult
+test = SurrogateAssociationTest(Zhu1(TEShannon()); nshuffles = 3) # not testing values, so few shuffles is ok
+@test independence(test, x, y, z) isa SurrogateAssociationTestResult
+
+test = SurrogateAssociationTest(Lindner(TEShannon()); nshuffles = 3)# not testing values, so few shuffles is ok
+@test independence(test, x, y, z) isa SurrogateAssociationTestResult
--- test/independence/SurrogateAssociationTest/TransferEntropyPairwise.jl
+++ test/independence/SurrogateAssociationTest/TransferEntropyPairwise.jl
@@ -1,14 +1,18 @@
 using Random
-rng = Random.MersenneTwister(1234)
-sys = system(Logistic2Unidir(; c_xy = 0.5, rng))
-x, y = columns(first(trajectory(sys, 1000, Ttr = 10000)))
+rng = Xoshiro(1234)
 
+sys = system(Logistic2Unidir(; c_xy = 0.5))
+x, y = columns(first(trajectory(sys, 200, Ttr = 10000)))
+
+# Creation
+@test SurrogateAssociationTest(est) isa SurrogateAssociationTest
+est = CMIDecomposition(TEShannon(), FPVP())
 # ArgumentError thrown if an estimator isn't provided.
 @test_throws ArgumentError SurrogateAssociationTest(TEShannon())
-@test SurrogateAssociationTest(TEShannon(), FPVP()) isa SurrogateAssociationTest
+
 
 α = 0.0287 # Arbitrary significance level 1 - α = 0.9713
-test = SurrogateAssociationTest(TEShannon(), FPVP(); rng)
+test = SurrogateAssociationTest(est; rng, nshuffles = 19)
 
 # The ground truth is X → Y, so we should be able to reject the null
 # when testing transferentropy(x → y)
@@ -19,5 +23,10 @@ test = SurrogateAssociationTest(TEShannon(), FPVP(); rng)
 @test pvalue(independence(test, y, x)) > α
 
 x, y = columns(first(trajectory(sys, 100, Ttr = 1000)))
-@test independence(SurrogateAssociationTest(TEShannon(), Lindner()), x, y) isa SurrogateAssociationTestResult
-@test independence(SurrogateAssociationTest(TEShannon(), Zhu1()), x, y) isa SurrogateAssociationTestResult
+est = Lindner()
+test = SurrogateAssociationTest(est, nshuffles = 19)
+@test independence(test, x, y) isa SurrogateAssociationTestResult
+
+est = Zhu1()
+test = SurrogateAssociationTest(est; nshuffles = 19)
+@test independence(test, x, y) isa SurrogateAssociationTestResult
