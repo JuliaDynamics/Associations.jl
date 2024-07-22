f2672a3c5f196f2bb6baf819b137876aa61a520b
Kristian Agasøster Haaga <kahaaga@gmail.com>
2024-07-22 09:54:53 +0200

    actually test recurrence-based methods

--- test/methods/methods.jl
+++ test/methods/methods.jl
@@ -2,6 +2,6 @@ testfile("closeness/closeness.jl")
 testfile("cross_mappings/ccm_like.jl")
 testfile("correlation/correlation.jl")
 testfile("information/information.jl")
-# testfile("recurrence/recurrence.jl")
+testfile("recurrence/recurrence.jl")
 
 testfile("deprecations.jl")
