dec74cbbc0923ed4cb2be443bb56b6bdc9698a43
Kristian Agasøster Haaga <kahaaga@gmail.com>
2024-07-22 10:06:17 +0200

    use rng

--- test/independence/SurrogateAssociationTest/crossmappings.jl
+++ test/independence/SurrogateAssociationTest/crossmappings.jl
@@ -1,7 +1,7 @@
 using Test
 using Random
 rng = MersenneTwister(1234)
-n = 200
+n = 250
 x, y = rand(rng, n), rand(rng, n)
 z = x.+ y
 
@@ -10,10 +10,10 @@ d = 2
 
 # Regular variant.
 est_ccm = RandomVectors(CCM(; d, τ); libsizes = 100, replace = true, rng)
-test_ccm = SurrogateAssociationTest(est_ccm; nshuffles = 19)
+test_ccm = SurrogateAssociationTest(est_ccm; rng, nshuffles = 19)
 
 est_pai = RandomVectors(PAI(; d, τ); libsizes = 100, replace = true, rng)
-test_pai = SurrogateAssociationTest(est_pai; nshuffles = 19)
+test_pai = SurrogateAssociationTest(est_pai; rng, nshuffles = 19)
 
 # Invalid syntax.
 @test_throws ArgumentError SurrogateAssociationTest(CCM(), RandomVectors(libsizes = 100:100:300))
@@ -27,8 +27,8 @@ test_pai = SurrogateAssociationTest(est_pai; nshuffles = 19)
 # Ensemble variant.
 eccm = Ensemble(RandomVectors(CCM(; d, τ); libsizes = 50, replace = true, rng))
 epai = Ensemble(RandomVectors(PAI(; d, τ); libsizes = 50, replace = true, rng))
-test_ccm = SurrogateAssociationTest(eccm; nshuffles = 19)
-test_pai = SurrogateAssociationTest(epai; nshuffles = 19)
+test_ccm = SurrogateAssociationTest(eccm; rng, nshuffles = 19)
+test_pai = SurrogateAssociationTest(epai; rng, nshuffles = 19)
 @test pvalue(independence(test_ccm, x, y)) > α
 @test pvalue(independence(test_ccm, x, z)) < α
 @test pvalue(independence(test_ccm, x, z)) < α
