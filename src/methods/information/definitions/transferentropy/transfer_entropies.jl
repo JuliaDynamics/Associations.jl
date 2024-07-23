6ead9bd61dbcecdf5ffcf069698309dbc4556b9f
Kristian Agasøster Haaga <kahaaga@gmail.com>
2024-07-23 15:35:47 +0200

    add method for JointProbabilities

--- src/methods/information/definitions/transferentropy/transfer_entropies.jl
+++ src/methods/information/definitions/transferentropy/transfer_entropies.jl
@@ -24,19 +24,37 @@ include("utils/OptimiseTraditional.jl")
 # to appropriately constructed marginals.
 function association(est::MultivariateInformationMeasureEstimator{<:TransferEntropy}, x...)
     te_definition = est.definition
-    
+    embedding = te_definition.embedding
     # If a conditional input (x[3]) is not provided, then C is just a 0-dimensional
     # StateSpaceSet. The horizontal concatenation of C with T then just returns T.
     # We therefore don't need separate methods for the conditional and non-conditional
     # cases.
-    S, T, T⁺, C = individual_marginals_te(te_definition.embedding, x...)
+    S, T, T⁺, C = individual_marginals_te(embedding, x...)
 
     cmi_est = convert_to_cmi_estimator(est)
+    if est isa JointProbabilities
+        # If discrete, we must codify each marginal separately and 
+        # collect the "marginal marginals" into new statespace sets.
+        tmp_TC = codify(est.discretization, StateSpaceSet(T, C))
+        if tmp_TC isa AbstractVector
+            T̂Ĉ = StateSpaceSet(tmp_TC)
+        else
+            T̂Ĉ = StateSpaceSet(tmp_TC...,)
+        end
+        Ŝ = codify(est.discretization, S)
+        T̂⁺ = codify(est.discretization, T⁺)
+        # We have already encoded the marginals, so when computing CMI, we can 
+        # simply use `UniqueElements`.
+        disc = CodifyVariables(UniqueElements())
+        est_unique = JointProbabilities(cmi_est.definition, disc, est.pest)
+        return association(est_unique, T̂⁺, Ŝ , T̂Ĉ)
 
-    # Estimate by letting TE(s -> t | c) := I(t⁺; s⁻ | t⁻, c⁻). 
-    return association(cmi_est, T⁺, S, StateSpaceSet(T, C))
+    else
+        #Estimate by letting TE(s -> t | c) := I(t⁺; s⁻ | t⁻, c⁻). 
+        return association(cmi_est, T⁺, S, StateSpaceSet(T, C))
+    end
+
 end
-
 
 function individual_marginals_te(emb::EmbeddingTE, x::VectorOr1DDataset...)
     joint, vars, τs, js = te_embed(emb, x...)
