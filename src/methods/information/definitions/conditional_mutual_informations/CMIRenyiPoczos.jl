07d907d14c8cc300e37502ae707e37b7efc563f4
Kristian Agasøster Haaga <kahaaga@gmail.com>
2024-07-23 12:27:41 +0200

    typo

--- src/methods/information/definitions/conditional_mutual_informations/CMIRenyiPoczos.jl
+++ src/methods/information/definitions/conditional_mutual_informations/CMIRenyiPoczos.jl
@@ -34,7 +34,6 @@ I_q^{R_{P}}(X; Y | Z) &= \\dfrac{1}{q-1}
 ## Estimation
 
 - [Example 1](@ref @id CMIRenyiPoczos_PoczosSchneiderCMI): Dedicated [`PoczosSchneiderCMI`](@ref) estimator.
-```
 """
 Base.@kwdef struct CMIRenyiPoczos{B, Q} <: ConditionalMutualInformation
     base::B = 2
