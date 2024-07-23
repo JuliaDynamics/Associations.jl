b5f8c26525d39f17b2ae6b29091c2245c63a6c7f
Kristian Agasøster Haaga <kahaaga@gmail.com>
2024-07-23 11:11:52 +0200

    Add tests

--- test/methods/information/conditional_entropies/ce_tsallis_abe.jl
+++ test/methods/information/conditional_entropies/ce_tsallis_abe.jl
@@ -24,3 +24,9 @@ est = JointProbabilities(ConditionalEntropyTsallisAbe(q = 0.5), disc);
 
 est = JointProbabilities(ConditionalEntropyTsallisAbe(q = 1.5), disc);
 @test association(est, X, Y) isa Real
+
+
+est_t = JointProbabilities(ConditionalEntropyTsallisAbe(q = 1.0), disc);
+est_s = JointProbabilities(ConditionalEntropyShannon(), disc);
+
+@test association(est_t, X, Y) == association(est_s, X, Y)
