2993d7322038d8bf5c4bdd13e6de29f437a05513
Kristian Agasøster Haaga <kahaaga@gmail.com>
2024-07-24 03:06:01 +0200

    remove deprecations

--- src/deprecations/deprecations.jl
+++ src/deprecations/deprecations.jl
@@ -1,7 +1,6 @@
 include("v3/deprecations_v3.jl")
 
 #include("transferentropy.jl")  # remove this file when 2.0 is released.
-include("crossmap.jl") # remove this file when 2.0 is released.
 #include("predictive_asymmetry.jl") # will be reintroduced in some 2.X release
 
 
