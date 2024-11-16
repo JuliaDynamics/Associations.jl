6837205db5ae34044a37927812efee19d6c0fe52
Kristian Agasøster Haaga <kahaaga@gmail.com>
2024-11-16 21:36:25 +0100

    move agreement test to utils

--- test/methods/information/transfer_entropies/te_renyi_jizba.jl
+++ test/methods/information/transfer_entropies/te_renyi_jizba.jl
@@ -1,6 +1,7 @@
 using Test
 using Associations
 using Random
+using StableRNGs
 
 rng = StableRNG(123)
 sys = system(Logistic4Chain(; rng))
@@ -49,13 +50,3 @@ out_hdisc = repr(EntropyDecomposition(te_def, PlugIn(Renyi()), CodifyVariables(V
 @test occursin("TEᵣⱼ(s → t | c) = hᵣ(t⁺, t⁻,c⁻) - hᵣ(t⁻,c⁻) - hᵣ(t⁺,s⁻,t⁻,c⁻) + hᵣ(s⁻,t⁻,c⁻)", out_hdiff)
 @test occursin("TEᵣⱼ(s → t | c) = Hᵣ(t⁺, t⁻,c⁻) - Hᵣ(t⁻,c⁻) - Hᵣ(t⁺,s⁻,t⁻,c⁻) + Hᵣ(s⁻,t⁻,c⁻)", out_hdisc)
 
-# Check if the difference is within a certain threshold percentage. Used to check 
-# agreement between `ValueBinning` and `TransferOperator` estimation.
-function in_agreement(val1, val2; agreement_threshold = 0.02)
-    largest_magnitude = max(abs(val1), abs(val2))    
-    if largest_magnitude == 0
-        return val1 == val2
-    else
-        return abs(val1 - val2) / largest_magnitude <= agreement_threshold
-    end   
-end
