7849b9d79d51e0279f49c80d26bfd5d6e5d74aac
Kristian Agasøster Haaga <kahaaga@gmail.com>
2025-08-12 15:36:20 +0200

    Better check for agreement between transfer operator and visitation frequency estimates
    Use reasonable number of points and percentage discrepancy

--- test/test_utils.jl
+++ test/test_utils.jl
@@ -1,10 +1,12 @@
 # Check if the difference is within a certain threshold percentage. Used to check 
 # agreement between `ValueBinning` and `TransferOperator` estimation.
-function in_agreement(val1, val2; agreement_threshold = 0.02)
-    largest_magnitude = max(abs(val1), abs(val2))    
+# `agreement_threshold` is a percentage indicating the maximal percentage 
+# discrepancy between the two values.
+function in_agreement(val1, val2; agreement_threshold=0.05)
+    largest_magnitude = max(abs(val1), abs(val2))
     if largest_magnitude == 0
         return val1 == val2
     else
         return abs(val1 - val2) / largest_magnitude <= agreement_threshold
-    end   
+    end
 end
