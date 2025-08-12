e377fe331fe18d84f61b32c85a55aedf0dae265e
Kristian Agasøster Haaga <kahaaga@gmail.com>
2025-08-12 17:37:24 +0200

    Use StableRNG for test that sometimes fails due to random data generation

--- test/independence/JointDistanceDistributionTest.jl
+++ test/independence/JointDistanceDistributionTest.jl
@@ -1,14 +1,14 @@
 using Random: MersenneTwister
-rng = MersenneTwister(12346)
+rng = StableRNG(12346)
 x, y = randn(rng, 1000), randn(rng, 1000)
-m = JointDistanceDistribution(D = 3, B = 5)
-test =  JointDistanceDistributionTest(m)
+m = JointDistanceDistribution(D=3, B=5)
+test = JointDistanceDistributionTest(m)
 @test test isa JointDistanceDistributionTest
 @test independence(test, x, y) isa Associations.JDDTestResult
 
 # Don't reject null at significance level (1 - α) when there is no coupling.
 α = 0.05
-@test pvalue(independence(test, x, y)) > α 
+@test pvalue(independence(test, x, y)) > α
 
 # Reject null at significance level (1 - α) when there is coupling
 z = y .+ x
--- test/independence/SurrogateAssociationTest/chatterjee_correlation.jl
+++ test/independence/SurrogateAssociationTest/chatterjee_correlation.jl
@@ -1,18 +1,19 @@
 using Test
 using Random
-rng = Xoshiro(1234)
+using StableRNGs
+rng = StableRNG(1234)
 
 # We can use surrogate tests and p-values to further verify the correctness of the 
 # algorithm.
-test = SurrogateAssociationTest(ChatterjeeCorrelation(), nshuffles = 19, rng = rng)
+test = SurrogateAssociationTest(ChatterjeeCorrelation(), nshuffles=19, rng=rng)
 
 # We expect that we *cannot* reject the null hypothesis for independent variables
 x = rand(rng, 1:10, 200)
 y = rand(rng, 1:10, 200)
 α = 0.05
-@test pvalue(independence(test, x, y)) > α 
+@test pvalue(independence(test, x, y)) > α
 
 # We expect that we *can* reject the null hypothesis for for dependent variables.
 w = rand(rng, 1:10, 200)
 z = rand(rng, 1:10, 200) .* sin.(w) .+ cos.(w)
-@test pvalue(independence(test, z, w)) < α 
+@test pvalue(independence(test, z, w)) < α
